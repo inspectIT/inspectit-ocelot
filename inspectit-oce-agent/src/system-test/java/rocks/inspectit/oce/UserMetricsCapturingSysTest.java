00943c85ffb13515d5d605fe9ea0fa957a673d9e
Jonas Kunz <Jonas.Kunz@novatec-gmbh.de>
2018-12-19 11:53:10 +0100

    Updated test and build script fixes.

---
+++ inspectit-oce-agent/src/system-test/java/rocks/inspectit/oce/UserMetricsCapturingSysTest.java
@@ -0,0 +1,37 @@
+package rocks.inspectit.oce;
+
+import io.opencensus.metrics.Metrics;
+import io.opencensus.metrics.export.MetricProducerManager;
+import io.opencensus.stats.*;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collections;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class UserMetricsCapturingSysTest {
+
+    private static final ViewManager viewManager = Stats.getViewManager();
+    private static final StatsRecorder statsRecorder = Stats.getStatsRecorder();
+
+    @Test
+    public void testUserMetricCapturing() {
+
+        Measure.MeasureLong myMeasure = Measure.MeasureLong.create("test_measure", "no actual meaning", "ms");
+        View myView = View.create(View.Name.create("test/view/test_measure"),
+                "test view", myMeasure,
+                Aggregation.Count.create(), Collections.emptyList());
+
+        viewManager.registerView(myView);
+        for (int i = 0; i < 42; i++) {
+            statsRecorder.newMeasureMap().put(myMeasure, 7L).record();
+        }
+
+        MetricProducerManager metricProducerManager = Metrics.getExportComponent().getMetricProducerManager();
+        System.out.println("sdafsdfsdf");
+        assertThat(metricProducerManager.getAllMetricProducer()).anyMatch(mp ->
+                mp.getMetrics().stream().filter(m -> m.getMetricDescriptor().getName() == "test/view/test_measure").count() == 1
+        );
+    }
+
+}
