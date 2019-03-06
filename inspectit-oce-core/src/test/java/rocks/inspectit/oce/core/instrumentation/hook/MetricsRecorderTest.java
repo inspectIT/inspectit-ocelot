29e9ce5c8f17b87cfda8cc9ba1e2a6fae8be3a2d
Jonas Kunz <Jonas.Kunz@gmx.net>
2019-03-06 16:05:17 +0100

    Fixed tests

--- inspectit-oce-core/src/test/java/rocks/inspectit/oce/core/instrumentation/hook/MetricsRecorderTest.java
+++ inspectit-oce-core/src/test/java/rocks/inspectit/oce/core/instrumentation/hook/MetricsRecorderTest.java
@@ -1,6 +1,5 @@
 package rocks.inspectit.oce.core.instrumentation.hook;
 
-import io.opencensus.stats.Measure;
 import io.opencensus.stats.MeasureMap;
 import io.opencensus.stats.StatsRecorder;
 import org.assertj.core.util.Maps;
@@ -10,13 +9,11 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Answers;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import rocks.inspectit.oce.core.metrics.MeasuresAndViewsManager;
 
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.Optional;
 
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
@@ -45,65 +42,34 @@ public class MetricsRecorderTest {
     class Execute {
 
         @Test
-        void verifyMissingConstantMetricIgnored() {
-            when(metricsManager.getMeasure(eq("my_metric"))).thenReturn(Optional.empty());
-            MetricsRecorder rec = new MetricsRecorder(Maps.newHashMap("my_metric", 42.0), Collections.emptyMap(), metricsManager, statsRecorder);
-
-            rec.execute(executionContext);
-
-            verify(measureMap, times(1)).record();
-            verify(measureMap, never()).put(any(Measure.MeasureLong.class), any(Long.class));
-            verify(measureMap, never()).put(any(Measure.MeasureDouble.class), any(Double.class));
-
-            Measure.MeasureLong measure = Mockito.mock(Measure.MeasureLong.class);
-            when(metricsManager.getMeasure(eq("my_metric"))).thenReturn(Optional.of(measure));
-
-
-            rec.execute(executionContext);
-
-            verify(measureMap, times(2)).record();
-            verify(measureMap, times(1)).put(any(Measure.MeasureLong.class), eq(42L));
-            verify(measureMap, never()).put(any(Measure.MeasureDouble.class), any(Double.class));
-        }
-
-
-        @Test
         void verifyNullValueDataMetricIgnored() {
 
-            when(metricsManager.getMeasure(eq("my_metric"))).thenReturn(Optional.empty());
-            when(executionContext.getInspectitContext().getData(eq("my_data"))).thenReturn(100);
+            when(executionContext.getInspectitContext().getData(eq("my_data"))).thenReturn(null);
 
             MetricsRecorder rec = new MetricsRecorder(Collections.emptyMap(), Maps.newHashMap("my_metric", "my_data"), metricsManager, statsRecorder);
 
             rec.execute(executionContext);
 
             verify(measureMap, times(1)).record();
-            verify(measureMap, never()).put(any(Measure.MeasureLong.class), any(Long.class));
-            verify(measureMap, never()).put(any(Measure.MeasureDouble.class), any(Double.class));
+            verify(metricsManager, never()).tryRecordingMeasurement(eq("my_metric"), same(measureMap), any(Number.class));
 
-            Measure.MeasureDouble measure = Mockito.mock(Measure.MeasureDouble.class);
-            when(metricsManager.getMeasure(eq("my_metric"))).thenReturn(Optional.of(measure));
-
+            when(executionContext.getInspectitContext().getData(eq("my_data"))).thenReturn(100L);
 
             rec.execute(executionContext);
 
             verify(measureMap, times(2)).record();
-            verify(measureMap, times(1)).put(any(Measure.MeasureDouble.class), eq(100.0));
-            verify(measureMap, never()).put(any(Measure.MeasureLong.class), any(Long.class));
+            verify(metricsManager, times(1)).tryRecordingMeasurement(eq("my_metric"), same(measureMap), any(Number.class));
+            verify(metricsManager, times(1)).tryRecordingMeasurement(eq("my_metric"), same(measureMap), eq((Number) 100L));
         }
 
 
         @Test
         void verifyInvalidDataTypeHandled() {
 
-            Measure.MeasureDouble measure1 = Mockito.mock(Measure.MeasureDouble.class);
-            Measure.MeasureDouble measure2 = Mockito.mock(Measure.MeasureDouble.class);
-            doReturn(Optional.of(measure1)).when(metricsManager).getMeasure(eq("my_metric1"));
-            lenient().doReturn(Optional.of(measure2)).when(metricsManager).getMeasure(eq("my_metric2"));
             when(executionContext.getInspectitContext().getData(any())).thenAnswer(invoc -> {
                 String dataKey = invoc.getArgument(0);
                 if ("my_data1".equals(dataKey)) {
-                    return 100;
+                    return 100.0;
                 } else if ("my_data2".equals(dataKey)) {
                     return "notanumber";
                 } else {
@@ -121,15 +87,15 @@ public class MetricsRecorderTest {
 
             verify(executionContext.getInspectitContext(), times(1)).getData(eq("my_data2"));
             verify(measureMap, times(1)).record();
-            verify(measureMap, times(1)).put(any(Measure.MeasureDouble.class), eq(100.0));
-            verify(measureMap, never()).put(any(Measure.MeasureLong.class), any(Long.class));
+            verify(metricsManager, times(1)).tryRecordingMeasurement(any(String.class), same(measureMap), any(Number.class));
+            verify(metricsManager, times(1)).tryRecordingMeasurement(eq("my_metric1"), same(measureMap), eq((Number) 100.0d));
 
             rec.execute(executionContext);
 
             verify(executionContext.getInspectitContext(), times(1)).getData(eq("my_data2"));
             verify(measureMap, times(2)).record();
-            verify(measureMap, times(2)).put(any(Measure.MeasureDouble.class), eq(100.0));
-            verify(measureMap, never()).put(any(Measure.MeasureLong.class), any(Long.class));
+            verify(metricsManager, times(2)).tryRecordingMeasurement(any(String.class), same(measureMap), any(Number.class));
+            verify(metricsManager, times(2)).tryRecordingMeasurement(eq("my_metric1"), same(measureMap), eq((Number) 100.0d));
         }
     }
 }
