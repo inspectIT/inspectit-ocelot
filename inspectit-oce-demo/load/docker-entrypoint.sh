2019167af1347fda4b0513dc735de0e95cf0544c
Alexander Wert <alexander.wert@novatec-gmbh.de>
2019-03-18 12:14:00 +0100

    added load generator

---
+++ inspectit-oce-demo/load/docker-entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/ash
+set -e
+
+until $(curl --output /dev/null --silent --head --fail http://api-gateway:8080); do
+  printf 'waiting for API Gateway ...\n'
+  sleep 10
+done
+
+sleep 30
+exec artillery "$@"
