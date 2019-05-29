02b44a70530bc483552081a0267382c083721ad8
Marius Oehler <marius.oehler@novatec-gmbh.de>
2019-05-29 17:04:29 +0200

    Added unit tests

---
+++ inspectit-ocelot-core/src/test/java/rocks/inspectit/ocelot/core/config/propertysources/http/HttpConfigurationPollerTest.java
@@ -0,0 +1,121 @@
+package rocks.inspectit.ocelot.core.config.propertysources.http;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import rocks.inspectit.ocelot.config.model.InspectitConfig;
+import rocks.inspectit.ocelot.config.model.config.ConfigSettings;
+import rocks.inspectit.ocelot.config.model.config.HttpConfigSettings;
+import rocks.inspectit.ocelot.core.config.InspectitEnvironment;
+
+import java.lang.reflect.Field;
+import java.time.Duration;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class HttpConfigurationPollerTest {
+
+    @InjectMocks
+    private HttpConfigurationPoller poller;
+
+    @Mock
+    private InspectitEnvironment env;
+
+    @Mock
+    private ScheduledExecutorService executor;
+
+    @Nested
+    public class DoEnable {
+
+        @Test
+        public void successfullyEnabled() {
+            InspectitConfig configuration = new InspectitConfig();
+            configuration.setConfig( new ConfigSettings());
+            configuration.getConfig().setHttp(new HttpConfigSettings());
+            configuration.getConfig().getHttp().setFrequency(Duration.ofMillis(5000L));
+            ScheduledFuture future = Mockito.mock(ScheduledFuture.class);
+            when(executor.scheduleWithFixedDelay(any(Runnable.class), anyLong(), anyLong(), any(TimeUnit.class))).thenReturn(future);
+
+            boolean result = poller.doEnable(configuration);
+
+            assertTrue(result);
+            verify(executor).scheduleWithFixedDelay(poller, 5000L, 5000L, TimeUnit.MILLISECONDS);
+            verifyNoMoreInteractions(executor);
+        }
+
+    }
+
+    @Nested
+    public class DoDisable {
+
+        @Test
+        public void notEnabled() {
+            boolean result = poller.doDisable();
+
+            assertTrue(result);
+        }
+
+        @Test
+        public void isEnabled() {
+            InspectitConfig configuration = new InspectitConfig();
+            configuration.setConfig( new ConfigSettings());
+            configuration.getConfig().setHttp(new HttpConfigSettings());
+            configuration.getConfig().getHttp().setFrequency(Duration.ofMillis(5000L));
+            ScheduledFuture future = Mockito.mock(ScheduledFuture.class);
+            when(executor.scheduleWithFixedDelay(any(Runnable.class), anyLong(), anyLong(), any(TimeUnit.class))).thenReturn(future);
+
+            poller.doEnable(configuration);
+
+            boolean result = poller.doDisable();
+
+            assertTrue(result);
+            verify(future).cancel(true);
+        }
+    }
+
+    @Nested
+    public class Run {
+
+        @Mock
+        private HttpPropertySourceState currentState;
+
+        @BeforeEach
+        public void beforeEach() throws Exception {
+            Field field = poller.getClass().getDeclaredField("currentState");
+            field.setAccessible(true);
+            field.set(poller, currentState);
+        }
+
+        @Test
+        public void stateNotUpdated() {
+            when(currentState.update()).thenReturn(false);
+
+            poller.run();
+
+            verify(currentState).update();
+            verifyNoMoreInteractions(currentState);
+            verifyZeroInteractions(env);
+        }
+
+        @Test
+        public void stateUpdated() {
+            when(currentState.update()).thenReturn(true);
+
+            poller.run();
+
+            verify(currentState).update();
+            verify(env).updatePropertySources(any());
+            verifyNoMoreInteractions(currentState, env);
+        }
+    }
+}
--- inspectit-ocelot-core/src/test/java/rocks/inspectit/ocelot/core/config/util/PropertyUtilsTest.java
+++ inspectit-ocelot-core/src/test/java/rocks/inspectit/ocelot/core/config/util/PropertyUtilsTest.java
@@ -1,5 +1,6 @@
 package rocks.inspectit.ocelot.core.config.util;
 
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.util.Properties;
@@ -8,18 +9,39 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class PropertyUtilsTest {
 
-    @Test
-    public void testJsonParsing() throws Exception {
-        String json = "{arr: [{x:42},{y:7},[\"A\",\"B\"]],nested:{name:\"blub\",\"complex.key\":true}}";
-        Properties result = PropertyUtils.readJson(json);
-        assertThat(result).hasSize(6);
-        assertThat(result).containsEntry("arr[0].x", 42);
-        assertThat(result).containsEntry("arr[1].y", 7);
-        assertThat(result).containsEntry("arr[2][0]", "A");
-        assertThat(result).containsEntry("arr[2][1]", "B");
-        assertThat(result).containsEntry("nested.name", "blub");
-        assertThat(result).containsEntry("nested.complex.key", true);
+    @Nested
+    public class ReadJson {
 
+        @Test
+        public void readJson() throws Exception {
+            String json = "{arr: [{x:42},{y:7},[\"A\",\"B\"]],nested:{name:\"blub\",\"complex.key\":true}}";
+
+            Properties result = PropertyUtils.readJson(json);
+
+            assertThat(result).hasSize(6);
+            assertThat(result).containsEntry("arr[0].x", 42);
+            assertThat(result).containsEntry("arr[1].y", 7);
+            assertThat(result).containsEntry("arr[2][0]", "A");
+            assertThat(result).containsEntry("arr[2][1]", "B");
+            assertThat(result).containsEntry("nested.name", "blub");
+            assertThat(result).containsEntry("nested.complex.key", true);
+
+        }
     }
 
+    @Nested
+    public class ReadYaml {
+
+        @Test
+        public void readYaml() {
+            String json = "root:\n  sub-child:\n    value: true\n  second: 42";
+
+            Properties result = PropertyUtils.readYaml(json);
+
+            assertThat(result).hasSize(2);
+            assertThat(result).containsEntry("root.sub-child.value", true);
+            assertThat(result).containsEntry("root.second", 42);
+
+        }
+    }
 }
