16a8066f8b4f635ddbb021e9a9100fe2c500b331
Marius Oehler <marius.oehler@novatec-gmbh.de>
2019-07-17 11:04:00 +0200

    Added comment

--- components/inspectit-ocelot-configurationserver-ui/src/lib/jwt-utils.js
+++ components/inspectit-ocelot-configurationserver-ui/src/lib/jwt-utils.js
@@ -1,5 +1,12 @@
 import jwtDecode from 'jwt-decode';
 
+/**
+ * Checks whether the given token is already expired.
+ * The function returns false if the token does not contain an expiration date.
+ * This function does not handle time sync issues between the server and client!
+ * 
+ * @param {string} token 
+ */
 export const isTokenExpired = (token) => {
     const { exp } = jwtDecode(token);
     const now = Date.now().valueOf() / 1000;
