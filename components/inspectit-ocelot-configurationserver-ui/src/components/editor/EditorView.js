aece23407f5888c3f252ac92083b8ee6178f46ba
Marius Oehler <mariusoe@users.noreply.github.com>
2019-07-24 16:25:08 +0200

    Refactoring of the EditorView component (#310)

--- components/inspectit-ocelot-configurationserver-ui/src/components/editor/EditorView.js
+++ components/inspectit-ocelot-configurationserver-ui/src/components/editor/EditorView.js
@@ -1,44 +1,18 @@
 import React from 'react'
-import { connect } from 'react-redux'
-import { configurationSelectors } from '../../redux/ducks/configuration'
-import { notificationActions } from '../../redux/ducks/notification'
-
-import dynamic from 'next/dynamic'
 import EditorToolbar from './EditorToolbar';
 
-import yamlEditorConfig from '../../data/yaml-editor-config.json'
+import dynamic from 'next/dynamic'
+const AceEditor = dynamic(() => import('./AceEditor'), { ssr: false });
 
-const AceEditor = dynamic(() => import('./AceEditor'), {
-    ssr: false
-});
+import editorConfig from '../../data/yaml-editor-config.json'
 
+/**
+ * Editor view consisting of the AceEditor and a toolbar.
+ */
 class EditorView extends React.Component {
 
-    initEditor = (editor) => {
-        this.editor = editor;
-    }
-
-    save = () => {
-        // save file
-    }
-
-    parsePath = (fullPath) => {
-        if (fullPath) {
-            const lastIndex = fullPath.lastIndexOf("/") + 1;
-            return {
-                path: fullPath.slice(0, lastIndex),
-                name: fullPath.slice(lastIndex)
-            }
-        } else {
-            return {};
-        }
-    }
-
     render() {
-        const { selection, isDirectory } = this.props;
-        const {path, name} = this.parsePath(selection);
-        const icon = "pi-" + (isDirectory ? "folder" : "file");
-        const enableButtons = !isDirectory && selection;
+        const { content, showEditor, hint, onSave, children } = this.props;
 
         return (
             <div className="this p-grid p-dir-col p-nogutter">
@@ -56,21 +30,20 @@ class EditorView extends React.Component {
                 `}</style>
                 <div className="p-col-fixed">
                     <EditorToolbar
-                        path={path}
-                        filename={name}
-                        enableButtons={enableButtons}
-                        icon={icon}
-                        onSave={this.save}
-                        onSearch={() => this.editor.execCommand("find")}
-                        onHelp={() => this.editor.showKeyboardShortcuts()} />
+                        enableButtons={showEditor}
+                        onSave={onSave}
+                        onSearch={() => this.editor.executeCommand("find")}
+                        onHelp={() => this.editor.showShortcuts()}>
+                        {children}
+                    </EditorToolbar>
                 </div>
                 <div className="p-col">
-                    {!selection || isDirectory ?
-                        <div className="selection-information">
-                            <div>Select a file to start editing.</div>
-                        </div>
+                    {showEditor ?
+                        <AceEditor editorRef={(editor) => this.editor = editor} mode="yaml" theme="cobalt" options={editorConfig} value={content} />
                         :
-                        <AceEditor mode="yaml" theme="cobalt" initEditor={this.initEditor} options={yamlEditorConfig} value={this.props.selection} />
+                        <div className="selection-information">
+                            <div>{hint}</div>
+                        </div>
                     }
                 </div>
             </div>
@@ -78,18 +51,7 @@ class EditorView extends React.Component {
     }
 }
 
-function mapStateToProps(state) {
-    const { selection } = state.configuration;
-    return {
-        selection,
-        isDirectory: configurationSelectors.isSelectionDirectory(state)
-    }
-}
 
-const mapDispatchToProps = {
-    showInfo: notificationActions.showInfoMessage
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(EditorView);
+export default EditorView;
 
 
