62fe20d6084b093ac1da655623d5c86a0d5b5d38
Jonas Kunz <Jonas.Kunz@gmx.net>
2019-07-24 14:21:20 +0200

    Added missing docu

--- components/inspectit-ocelot-configurationserver/src/main/java/rocks/inspectit/ocelot/rest/agentstatus/AgentStatusController.java
+++ components/inspectit-ocelot-configurationserver/src/main/java/rocks/inspectit/ocelot/rest/agentstatus/AgentStatusController.java
@@ -1,5 +1,6 @@
 package rocks.inspectit.ocelot.rest.agentstatus;
 
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,11 +23,13 @@ public class AgentStatusController extends AbstractBaseController {
     @Autowired
     private AgentStatusManager statusManager;
 
+    @ApiOperation(value = "Fetch the List of Agent Statuses", notes = "Gives a list of connected agents")
     @GetMapping(value = "agentstatus")
     public Collection<AgentStatus> fetchAgentStatuses(@RequestParam Map<String, String> attributes) {
         return statusManager.getAgentStatuses();
     }
 
+    @ApiOperation(value = "Clear the List of Agent Statuses", notes = "Clears the list of connected agents")
     @DeleteMapping(value = "agentstatus")
     public void clearAgentStatuses(@RequestParam Map<String, String> attributes) {
         statusManager.reset();
