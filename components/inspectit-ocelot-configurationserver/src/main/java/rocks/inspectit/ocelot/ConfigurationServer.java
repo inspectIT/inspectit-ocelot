8e9efee481e493f09b0ddbf0b87f84eab38ab089
Jonas Kunz <Jonas.Kunz@gmx.net>
2019-07-31 12:18:32 +0200

    Closes #323 - Ensured working directory is created before spring context is initialized

--- components/inspectit-ocelot-configurationserver/src/main/java/rocks/inspectit/ocelot/ConfigurationServer.java
+++ components/inspectit-ocelot-configurationserver/src/main/java/rocks/inspectit/ocelot/ConfigurationServer.java
@@ -1,16 +1,38 @@
 package rocks.inspectit.ocelot;
 
-import org.springframework.boot.SpringApplication;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.FileUtils;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.properties.bind.Binder;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import rocks.inspectit.ocelot.config.model.InspectitServerSettings;
+
+import java.io.File;
 
 /**
  * The application class of the configuration server.
  */
 @SpringBootApplication
+@Slf4j
 public class ConfigurationServer {
 
+    private static final ApplicationContextInitializer<ConfigurableApplicationContext> WORKDIR_CREATOR = (ctx) -> {
+        InspectitServerSettings settings = Binder
+                .get(ctx.getEnvironment())
+                .bind("inspectit", InspectitServerSettings.class).get();
+        try {
+            FileUtils.forceMkdir(new File(settings.getWorkingDirectory()));
+        } catch (Exception e) {
+            log.error("Could not create working directory", e);
+        }
+    };
+
     public static void main(String[] args) {
-        SpringApplication.run(ConfigurationServer.class, args);
+        new SpringApplicationBuilder(ConfigurationServer.class)
+                .initializers(WORKDIR_CREATOR)
+                .run(args);
     }
 
 }
