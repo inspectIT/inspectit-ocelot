c81ed0b86b00a0541269b76dda30fcd009c89b61
Marius Oehler <marius.oehler@novatec-gmbh.de>
2019-08-14 16:27:41 +0200

    Remove example files from configuration server UI project

--- components/inspectit-ocelot-configurationserver-ui/src/redux/ducks/clock/types.js
+++
@@ -1,5 +0,0 @@
-export const INIT = 'clock/INIT';
-export const TICK = 'clock/TICK';
-export const INCREMENT = 'clock/INCREMENT';
-export const DECREMENT = 'clock/DECREMENT';
-export const RESET = 'clock/RESET';
