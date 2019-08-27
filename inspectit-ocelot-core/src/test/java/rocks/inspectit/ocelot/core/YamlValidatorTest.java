9bd9e89418894bc885a560349653fc88830e3cea
mbrill-nt <marius.brill@novatec-gmbh.de>
2019-08-27 10:51:09 +0200

    Improved YamlValidatorTest

--- inspectit-ocelot-core/src/test/java/rocks/inspectit/ocelot/core/YamlValidatorTest.java
+++ inspectit-ocelot-core/src/test/java/rocks/inspectit/ocelot/core/YamlValidatorTest.java
@@ -3,19 +3,18 @@ package rocks.inspectit.ocelot.core;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import rocks.inspectit.ocelot.config.model.InspectitConfig;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class YamlValidatorTest {
 
-    @Autowired
     YamlValidator validator;
 
     @BeforeEach
@@ -28,62 +27,132 @@ public class YamlValidatorTest {
 
         @Test
         void kebabCaseTest() {
-            LinkedList<String> output = new LinkedList<>(Arrays.asList("ich-kann-kebab", "case", "sogar-mit-klammern\\wow", "sachen-gibts"));
-            assertEquals(output, validator.parse("inspectit.ich-kann-kebab.case[sogar-mit-klammern\\wow].sachen-gibts"));
+            ArrayList<String> output = new ArrayList<>(Arrays.asList("inspectit", "iCan-parse-kebab", "case", "even-in-brackets\\wow", "thisIs-awesome"));
+            assertThat(validator.parse("inspectit.iCan-parse-kebab.case[even-in-brackets\\wow].thisIs-awesome")).isEqualTo(output);
         }
 
         @Test
         void emptyString() {
-            LinkedList<String> output = new LinkedList<>();
+            ArrayList<String> output = new ArrayList<>();
             assertThat(validator.parse("")).isEqualTo(output);
         }
 
         @Test
         void nullString() {
-            LinkedList<String> output = new LinkedList<>();
+            ArrayList<String> output = new ArrayList<>();
             assertThat(validator.parse(null)).isEqualTo(output);
+
+        }
+
+        @Test
+        void bracketAfterBracket() {
+            ArrayList<String> output = new ArrayList<>(Arrays.asList("inspectit", "property", "first", "second"));
+            assertThat(validator.parse("inspectit.property[first][second]")).isEqualTo(output);
+        }
+
+        @Test
+        void dotInBrackets() {
+            ArrayList<String> output = new ArrayList<>(Arrays.asList("inspectit", "property", "first.second"));
+            assertThat(validator.parse("inspectit.property[first.second]")).isEqualTo(output);
+        }
+
+        @Test
+        void throwsException() {
+            try {
+                validator.parse("inspectit.property[first.second");
+            } catch (IllegalArgumentException e) {
+                assertThat(e.getMessage().equals("invalid property path")).isEqualTo(true);
+            }
         }
 
     }
 
     @Nested
-    public class FindUnmappedStrings {
+    public class CheckPropertyName {
+        @Test
+        void wrongProperty() {
+            String property = "inspectit.iDoNotExist";
+            assertThat(validator.checkPropertyName(property)).isEqualTo(true);
+        }
 
         @Test
-        void workingTest() {
-            String[] strings = {"irgendwasAnderes"/*Alles was nicht mit inspectit. beginnt wird ignoriert*/, "inspectit.config.das-geht-nie-imLeben", "inspectit.metrics.processor.enabled",
-                    "inspectit.metrics.processor.enbaled"};
-            ArrayList<String> output = new ArrayList<>(Arrays.asList("inspectit.config.das-geht-nie-imLeben", "inspectit.metrics.processor.enbaled"));
-            assertEquals(output, validator.findInvalidPropertyNames(strings));
+        void correctProperty() {
+            String property = "inspectit.service-name";
+            assertThat(validator.checkPropertyName(property)).isEqualTo(false);
         }
 
         @Test
         void emptyString() {
-            String[] strings = {"inspectit.config.das-geht-nie-imLeben", "", "inspectit.metrics.processor.enabled"};
-            ArrayList<String> output = new ArrayList<>(Arrays.asList("inspectit.config.das-geht-nie-imLeben"));
-            assertEquals(output, validator.findInvalidPropertyNames(strings));
+            String property = "";
+            assertThat(validator.checkPropertyName(property)).isEqualTo(false);
         }
 
         @Test
-        void nullString() {
-            String[] strings = {"inspectit.config.das-geht-nie-imLeben", null, "inspectit.metrics.processor.enabled"};
-            ArrayList<String> output = new ArrayList<>(Arrays.asList("inspectit.config.das-geht-nie-imLeben"));
-            assertEquals(output, validator.findInvalidPropertyNames(strings));
+        void noneInspectitInput() {
+            String property = "thisHasNothingToDoWithInspectit";
+            assertThat(validator.checkPropertyName(property)).isEqualTo(false);
+        }
+    }
+
+    @Nested
+    public class CheckPropertyExists {
+
+        @Test
+        void workingTest() {
+            ArrayList<String> list = new ArrayList<>(Arrays.asList("exporters", "metrics", "prometheus"));
+            assertThat(validator.checkPropertyExists(list, InspectitConfig.class)).isEqualTo(true);
         }
 
         @Test
-        void existingMap() {
-            String[] strings = {"inspectit.metrics.definitions[jvm/gc/concurrent/phase/time].description"};
-            ArrayList<String> output = new ArrayList<>();
-            assertEquals(output, validator.findInvalidPropertyNames(strings));
+        void emptyString() {
+            ArrayList<String> list = new ArrayList<>(Arrays.asList(""));
+            assertThat(validator.checkPropertyExists(list, InspectitConfig.class)).isEqualTo(false);
         }
 
         @Test
         void existingList() {
-            String[] strings = {"inspectit.instrumentation.scopes.jdbc_statement_execute.interfaces[0].matcher-mode"};
-            ArrayList<String> output = new ArrayList<>();
-            assertEquals(output, validator.findInvalidPropertyNames(strings));
+            ArrayList<String> list = new ArrayList<>(Arrays.asList("instrumentation", "scopes", "jdbc_statement_execute", "interfaces", "0", "matcher-mode"));
+            assertThat(validator.checkPropertyExists(list, InspectitConfig.class)).isEqualTo(true);
+        }
+
+        @Test
+        void existingMap() {
+            ArrayList<String> list = new ArrayList<>(Arrays.asList("metrics", "definitions", "jvm/gc/concurrent/phase/time", "description"));
+            assertThat(validator.checkPropertyExists(list, InspectitConfig.class)).isEqualTo(true);
+        }
+
+        @Test
+        void readMethodIsNull() {
+            ArrayList<String> list = new ArrayList<>(Arrays.asList("inspectit", "instrumentation", "data", "method_duration", "is-tag"));
+            assertThat(validator.checkPropertyExists(list, InspectitConfig.class)).isEqualTo(true);
+        }
+
+        @Test
+        void endsInWildcardType() {
+            ArrayList<String> list = new ArrayList<>(Arrays.asList("inspectit", "instrumentation", "actions", "string_replace_all", "input", "regex"));
+            assertThat(validator.checkPropertyExists(list, InspectitConfig.class)).isEqualTo(true);
         }
     }
 
+    @Nested
+    public class CheckPropertyExistsInMap {
+
+        @Test
+        void mapTest() {
+            ArrayList<String> list = new ArrayList<>(Arrays.asList("matcher-mode"));
+            assertThat(validator.checkPropertyExistsInMap(list, Map.class)).isEqualTo(true);
+
+        }
+    }
+
+
+    @Nested
+    public class CheckPropertyExistsInList {
+
+        @Test
+        void listTest() {
+            ArrayList<String> list = new ArrayList(Arrays.asList("matcher-mode"));
+            assertThat(validator.checkPropertyExistsInList(list, List.class)).isEqualTo(true);
+        }
+    }
 }
