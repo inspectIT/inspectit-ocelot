866ff8a66392bb403f368dc9ab3e79103d341cf0
Marius Brill <53812669+mbrill-nt@users.noreply.github.com>
2019-09-10 10:01:18 +0200

    Closes #379 - Add an autocompletion service providing model-based suggestions

---
+++ inspectit-ocelot-config/src/main/java/rocks/inspectit/ocelot/config/validation/PropertyPathHelper.java
@@ -0,0 +1,247 @@
+package rocks.inspectit.ocelot.config.validation;
+
+import lombok.experimental.UtilityClass;
+import org.springframework.beans.BeanUtils;
+import org.springframework.core.io.FileSystemResource;
+import rocks.inspectit.ocelot.config.utils.CaseUtils;
+
+import java.beans.PropertyDescriptor;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.net.URL;
+import java.nio.file.Path;
+import java.time.Duration;
+import java.util.*;
+
+@UtilityClass
+public class PropertyPathHelper {
+
+    /**
+     * A HashSet of classes which are used as wildcards in the search for properties. If a found class matches one of these
+     * classes, the end of the property path is reached. Mainly used in the search of maps
+     */
+    private static final HashSet<Class<?>> TERMINAL_TYPES = new HashSet(Arrays.asList(Object.class, String.class, Integer.class, Long.class,
+            Float.class, Double.class, Character.class, Void.class,
+            Boolean.class, Byte.class, Short.class, Duration.class, Path.class, URL.class, FileSystemResource.class));
+
+    /**
+     * Returns the type which can be found at the end of the path. Returns null if the path does not exist
+     *
+     * @param propertyNames The list of properties one wants to check
+     * @param type          The type in which the current top-level properties should be found
+     * @return The type which can be found at the end of the path. Returns null if the path does not exist
+     */
+    public Type getPathEndType(List<String> propertyNames, Type type) {
+        if (propertyNames.isEmpty()) {
+            return type;
+        }
+        if (type instanceof ParameterizedType) {
+            ParameterizedType genericType = (ParameterizedType) type;
+            if (genericType.getRawType() == Map.class) {
+                return getTypeInMap(propertyNames, genericType.getActualTypeArguments()[1]);
+            } else if (genericType.getRawType() == List.class) {
+                return getTypeInList(propertyNames, genericType.getActualTypeArguments()[0]);
+            }
+        }
+        if (type instanceof Class) {
+            return getTypeInBean(propertyNames, (Class<?>) type);
+        } else {
+            throw new IllegalArgumentException("Unexpected type: " + type);
+        }
+    }
+
+    /**
+     * Checks if a given type exists as value type in a map, keeps crawling through the given propertyName list
+     *
+     * @param propertyNames List of property names
+     * @param mapValueType  The type which is given as value type of a map
+     * @return True: The type exists <br> False: the type does not exists
+     */
+    Type getTypeInMap(List<String> propertyNames, Type mapValueType) {
+        if (isTerminal(mapValueType)) {
+            return mapValueType;
+        } else {
+            return getPathEndType(propertyNames.subList(1, propertyNames.size()), mapValueType);
+        }
+    }
+
+    /**
+     * Checks if a given type exists as value type in a list, keeps crawling through the given propertyName list
+     *
+     * @param propertyNames List of property names
+     * @param listValueType The type which is given as value type of a list
+     * @return True: The type exists <br> False: the type does not exists
+     */
+    Type getTypeInList(List<String> propertyNames, Type listValueType) {
+        return getPathEndType(propertyNames.subList(1, propertyNames.size()), listValueType);
+    }
+
+    /**
+     * Checks if the first entry of the propertyNames list exists as property in a given bean
+     *
+     * @param propertyNames List of property names
+     * @param beanType      The bean through which should be searched
+     * @return True: the property and all other properties exists <br> False: At least one of the properties does not exist
+     */
+    private Type getTypeInBean(List<String> propertyNames, Class<?> beanType) {
+        String propertyName = CaseUtils.kebabCaseToCamelCase(propertyNames.get(0));
+        Optional<PropertyDescriptor> foundProperty =
+                Arrays.stream(BeanUtils.getPropertyDescriptors(beanType))
+                        .filter(descriptor -> CaseUtils.compareIgnoreCamelOrKebabCase(propertyName, descriptor.getName()))
+                        .findFirst();
+        if (foundProperty.isPresent()) {
+            Type propertyType;
+            if (foundProperty.get().getReadMethod() != null) {
+                propertyType = foundProperty.get().getReadMethod().getGenericReturnType();
+            } else {
+                propertyType = foundProperty.get().getPropertyType();
+            }
+            return getPathEndType(propertyNames.subList(1, propertyNames.size()), propertyType);
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * Checks if a given type is a terminal type or an enum
+     *
+     * @param type
+     * @return True: the given type is a terminal or an enum False: the given type is neither a terminal type nor an enum
+     */
+    public boolean isTerminal(Type type) {
+        if (TERMINAL_TYPES.contains(type)) {
+            return true;
+        } else if (type instanceof Class) {
+            return ((Class<?>) type).isEnum() || ((Class<?>) type).isPrimitive();
+        }
+        return false;
+    }
+
+    /**
+     * Checks if a given type is a list of terminal types
+     *
+     * @param type
+     * @return True: the given type is a list of a terminal type. False: either the given type is not a list or not a list of terminal types
+     */
+    public boolean isListOfTerminalTypes(Type type) {
+        if (type instanceof ParameterizedType) {
+            ParameterizedType genericType = (ParameterizedType) type;
+            if (genericType.getRawType() == List.class) {
+                return isTerminal(genericType.getActualTypeArguments()[0]);
+            }
+        }
+        return false;
+    }
+
+
+    /**
+     * This method takes an array of strings and returns each entry as ArrayList containing the parts of each element.
+     * <p>
+     * 'inspectit.hello-i-am-testing' would be returned as {'inspectit', 'helloIAmTesting'}
+     *
+     * @param propertyName A String containing the property path
+     * @return a List containing containing the parts of the property path as String
+     */
+    public List<String> parse(String propertyName) {
+        ArrayList<String> result = new ArrayList<>();
+        String remainder = propertyName;
+        while (remainder != null && !remainder.isEmpty()) {
+            remainder = extractExpression(remainder, result);
+        }
+        return result;
+    }
+
+    /**
+     * Extracts the first path expression from the given propertyName and appends it to the given result list.
+     * The remainder of the property name is returned
+     * <p>
+     * E.g. inspectit.test.rest -> "inspectit" is added to the list, "test.rest" is returned.
+     * E.g. [inspectit.literal].test.rest -> "inspectit.literal" is added to the list, "test.rest" is returned.
+     * E.g. [inspectit.literal][test].rest -> "inspectit.literal" is added to the list, "[test].rest" is returned.
+     *
+     * @param propertyName A String with the path of a property
+     * @param result       Reference to the list in which the extracted expressions should be saved in
+     * @return the remaining expression
+     */
+    private String extractExpression(String propertyName, List<String> result) {
+        if (propertyName.startsWith("[")) {
+            int end = propertyName.indexOf(']');
+            if (end == -1) {
+                throw new IllegalArgumentException("invalid property path");
+            }
+            result.add(propertyName.substring(1, end));
+            return removeLeadingDot(propertyName.substring(end + 1));
+        } else {
+            int end = findFirstIndexOf(propertyName, '.', '[');
+            if (end == -1) {
+                result.add(propertyName);
+                return "";
+            } else {
+                result.add(propertyName.substring(0, end));
+                return removeLeadingDot(propertyName.substring(end));
+            }
+        }
+    }
+
+    private int findFirstIndexOf(String propertyName, char first, char second) {
+        int firstIndex = propertyName.indexOf(first);
+        int secondIndex = propertyName.indexOf(second);
+        if (firstIndex == -1) {
+            return secondIndex;
+        } else if (secondIndex == -1) {
+            return firstIndex;
+        } else {
+            return Math.min(firstIndex, secondIndex);
+        }
+    }
+
+    private String removeLeadingDot(String string) {
+        if (string.startsWith(".")) {
+            return string.substring(1);
+        } else {
+            return string;
+        }
+    }
+
+
+    /**
+     * Checks if two paths are the same. If one path uses the Wildcard "*", the check with the corresponding literal in the
+     * other path return true.
+     *
+     * @param pathA the first path to be compared
+     * @param pathB the second path to be compared
+     * @return
+     */
+    public boolean comparePaths(List<String> pathA, List<String> pathB) {
+        if (pathA == null || pathB == null || pathA.size() != pathB.size()) {
+            return false;
+        }
+        for (int i = 0; i < pathA.size(); i++) {
+            String pathALiteral = pathA.get(i);
+            String pathBLiteral = pathB.get(i);
+            if (!(pathALiteral.equals("*") || pathBLiteral.equals("*") || CaseUtils.compareIgnoreCamelOrKebabCase(pathBLiteral, pathALiteral))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    /**
+     * Checks if the first given path starts with the second given full path
+     * <p>
+     * Example: pathA = {"inspectit","instrumentation","rules"}
+     * pathB = {"inspectit", "instrumentation"}
+     * would return true
+     *
+     * @param path   The path you want to check
+     * @param prefix The prefix the other path should begin with
+     * @return
+     */
+    public boolean hasPathPrefix(List<String> path, List<String> prefix) {
+        if (prefix.size() > path.size()) {
+            return false;
+        }
+        return comparePaths(path.subList(0, prefix.size()), prefix);
+    }
+}
--- inspectit-ocelot-core/src/test/java/rocks/inspectit/ocelot/core/config/PropertyNamesValidatorTest.java
+++ inspectit-ocelot-core/src/test/java/rocks/inspectit/ocelot/core/config/PropertyNamesValidatorTest.java
@@ -3,12 +3,6 @@ package rocks.inspectit.ocelot.core.config;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import rocks.inspectit.ocelot.config.model.InspectitConfig;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -23,169 +17,39 @@ public class PropertyNamesValidatorTest {
     }
 
     @Nested
-    public class Parse {
-        @Test
-        void kebabCaseTest() {
-            List<String> output = Arrays.asList("inspectit", "iCan-parse-kebab", "case", "even-in-brackets\\wow", "thisIs-awesome");
-
-            assertThat(validator.parse("inspectit.iCan-parse-kebab.case[even-in-brackets\\wow].thisIs-awesome")).isEqualTo(output);
-        }
-
-        @Test
-        void emptyString() {
-            ArrayList<String> output = new ArrayList<>();
-
-            assertThat(validator.parse("")).isEqualTo(output);
-        }
-
-        @Test
-        void nullString() {
-            ArrayList<String> output = new ArrayList<>();
-
-            assertThat(validator.parse(null)).isEqualTo(output);
-
-        }
-
-        @Test
-        void bracketAfterBracket() {
-            List<String> output = Arrays.asList("inspectit", "property", "first", "second");
-
-            assertThat(validator.parse("inspectit.property[first][second]")).isEqualTo(output);
-        }
-
-        @Test
-        void dotInBrackets() {
-            List<String> output = Arrays.asList("inspectit", "property", "first.second");
-
-            assertThat(validator.parse("inspectit.property[first.second]")).isEqualTo(output);
-        }
-
-        @Test
-        void throwsException() {
-            try {
-                validator.parse("inspectit.property[first.second");
-            } catch (IllegalArgumentException e) {
-                assertThat(e.getMessage()).isEqualTo("invalid property path");
-            }
-        }
-
-    }
-
-    @Nested
-    public class IsInvalidPropertyName {
+    public class CheckPropertyName {
         @Test
         void wrongProperty() {
             String property = "inspectit.iDoNotExist";
 
-            assertThat(validator.isInvalidPropertyName(property)).isEqualTo(true);
+            assertThat(validator.checkPropertyName(property)).isEqualTo(false);
         }
 
         @Test
-        void correctProperty() {
+        void correctPropertyButNotAtPathEnd() {
             String property = "inspectit.service-name";
 
-            assertThat(validator.isInvalidPropertyName(property)).isEqualTo(false);
+            assertThat(validator.checkPropertyName(property)).isEqualTo(true);
         }
 
         @Test
         void emptyString() {
             String property = "";
 
-            assertThat(validator.isInvalidPropertyName(property)).isEqualTo(false);
+            assertThat(validator.checkPropertyName(property)).isEqualTo(false);
         }
 
         @Test
         void noneInspectitInput() {
             String property = "thisHasNothingToDoWithInspectit";
 
-            assertThat(validator.isInvalidPropertyName(property)).isEqualTo(false);
-        }
-    }
-
-    @Nested
-    public class CheckPropertyExists {
-        @Test
-        void termminalTest() {
-            List<String> list = Arrays.asList("config", "file-based", "path");
-
-            assertThat(validator.checkPropertyExists(list, InspectitConfig.class)).isEqualTo(true);
-        }
-
-        @Test
-        void nonTermminalTest() {
-            List<String> list = Arrays.asList("exporters", "metrics", "prometheus");
-
-            assertThat(validator.checkPropertyExists(list, InspectitConfig.class)).isEqualTo(false);
-        }
-
-        @Test
-        void emptyString() {
-            List<String> list = Arrays.asList("");
-
-            assertThat(validator.checkPropertyExists(list, InspectitConfig.class)).isEqualTo(false);
-        }
-
-        @Test
-        void existingList() {
-            List<String> list = Arrays.asList("instrumentation", "scopes", "jdbc_statement_execute", "interfaces", "0", "matcher-mode");
-
-            assertThat(validator.checkPropertyExists(list, InspectitConfig.class)).isEqualTo(true);
-        }
-
-        @Test
-        void existingMap() {
-            List<String> list = Arrays.asList("metrics", "definitions", "jvm/gc/concurrent/phase/time", "description");
-
-            assertThat(validator.checkPropertyExists(list, InspectitConfig.class)).isEqualTo(true);
-        }
-
-        @Test
-        void readMethodIsNull() {
-            List<String> list = Arrays.asList("instrumentation", "data", "method_duration", "is-tag");
-
-            assertThat(validator.checkPropertyExists(list, InspectitConfig.class)).isEqualTo(true);
+            assertThat(validator.checkPropertyName(property)).isEqualTo(false);
         }
 
         @Test
         void endsInWildcardType() {
-            List<String> list = Arrays.asList("instrumentation", "actions", "string_replace_all", "input", "regex");
 
-            assertThat(validator.checkPropertyExists(list, InspectitConfig.class)).isEqualTo(true);
-        }
-
-        @Test
-        void terminalListTest() {
-            List<String> list = Arrays.asList("inspectit", "instrumentation", "scopes", "httpurlconnection_getInputStream.methods", "0", "arguments");
-
-            assertThat(validator.checkPropertyExists(list, InspectitConfig.class)).isEqualTo(false);
-        }
-
-        @Test
-        void nonInspectItInput() {
-            List<String> list = Arrays.asList("java", "specification", "version");
-
-            assertThat(validator.checkPropertyExists(list, InspectitConfig.class)).isEqualTo(false);
-        }
-    }
-
-    @Nested
-    public class CheckPropertyExistsInMap {
-        @Test
-        void nonTerminalMapTest() {
-            List<String> list = Arrays.asList("matcher-mode");
-
-            assertThat(validator.checkPropertyExistsInMap(list, Map.class)).isEqualTo(false);
-
-        }
-    }
-
-    @Nested
-    public class CheckPropertyExistsInList {
-        @Test
-        void nonTerminalListTest() {
-            List<String> list = Arrays.asList("matcher-mode");
-
-            assertThat(validator.checkPropertyExistsInList(list, List.class)).isEqualTo(false);
+            assertThat(validator.checkPropertyName("inspectit.instrumentation.actions.string_replace_all.input.regex")).isEqualTo(true);
         }
     }
 }
