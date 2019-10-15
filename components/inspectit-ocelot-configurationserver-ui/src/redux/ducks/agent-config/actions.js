129359bde1867e97c11b4ee1b9ffb048297709ce
Ariane <ariane.kraus@hotmail.de>
2019-10-15 13:16:43 +0200

    added randexp dependency which will convert attr. value Regexp to matching strings

--- components/inspectit-ocelot-configurationserver-ui/src/redux/ducks/agent-config/actions.js
+++ components/inspectit-ocelot-configurationserver-ui/src/redux/ducks/agent-config/actions.js
@@ -1,15 +1,23 @@
 import axios from '../../../lib/axios-api';
 import { notificationActions } from '../notification';
 import yaml from 'js-yaml';
+import RandExp from 'randexp';
 
+/**
+ * pretends to be an agent and requests the configuration for the given attributes
+ * and will start a download dialog afterwards
+ * 
+ * @param {obj} attributes - the attribute object for which the configuration shall be downloaded
+ */
 export const fetchConfigurationFile = (attributes) => {
 	let fileName = 'agent-config'
+	const solvedAtts = _solveRegexValues(attributes)
 
 	return dispatch => {
 		axios
 		.get('/agent/configuration', {
 			params: {
-				...attributes
+				...solvedAtts
 			}
 		})
 		.then(res => {
@@ -33,5 +41,16 @@ export const fetchConfigurationFile = (attributes) => {
 				link.click();
 			}
 		})
+		.catch(() => {
+			dispatch(notificationActions.showInfoMessage("Downloading config file failed", "No file could have been retrieved. There might not be a configuration for the given attributes"));
+		})
 	}
+}
+
+const _solveRegexValues = (obj) => {
+	let res = {}
+	Object.keys(obj).map(key => {
+		res[key] = new RandExp(obj[key]).gen()
+	})
+	return res
 }
