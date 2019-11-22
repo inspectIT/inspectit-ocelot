17faaf2a5a156001b2d61896abe59068709d073e
Marius Oehler <mariusoe@users.noreply.github.com>
2019-11-22 15:04:24 +0100

    Closes #467 -  Show version number at login screen of configuration server (#471)

--- components/inspectit-ocelot-configurationserver-ui/src/components/views/LoginView.js
+++ components/inspectit-ocelot-configurationserver-ui/src/components/views/LoginView.js
@@ -14,9 +14,21 @@ const LoginView = (props) => {
                 align-items: center;
                 height: 100vh;
             }
+            .build-information {
+                position: absolute;
+                bottom: .5rem;
+                font-size: .75rem;
+                color: #888;
+            }
+            .build-information a {
+                color: #888;
+            }
             `}</style>
             {props.children}
             <LoginCard />
+            <div className="build-information">
+                <span>inspectIT Ocelot Configuration Server v{process.env.VERSION} (Build Date: {process.env.BUILD_DATE}) | <a target="_blank" href="http://docs.inspectit.rocks/">Docs</a> | <a target="_blank" href="https://github.com/inspectIT/inspectit-ocelot">Github</a></span>
+            </div>
         </div>
     )
 }
