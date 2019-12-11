2004779c99d49288bcf841fdfb3c8ffaf5c1acd2
mbrill-nt <marius.brill@novatec-gmbh.de>
2019-12-11 17:29:29 +0100

    Added changes from code review

--- components/inspectit-ocelot-configurationserver/src/main/resources/db/migration/V1__init.sql
+++ components/inspectit-ocelot-configurationserver/src/main/resources/db/migration/V1__init.sql
@@ -1,2 +1,3 @@
 CREATE TABLE hibernate_sequence (next_val bigint);
 INSERT INTO hibernate_sequence (next_val) VALUES (0);
+CREATE TABLE user (id bigint not null, is_ldap_user boolean not null, password_hash varchar(255) not null, username varchar(255) not null unique, primary key (id));
--- components/inspectit-ocelot-configurationserver/src/main/resources/db/migration/V2__users.sql
+++
@@ -1,1 +0,0 @@
-CREATE TABLE user (id bigint not null, is_ldap_user boolean not null, password_hash varchar(255) not null, username varchar(255) not null unique, primary key (id));
