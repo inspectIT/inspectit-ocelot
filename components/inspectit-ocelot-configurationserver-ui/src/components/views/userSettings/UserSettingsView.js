9a212571ca3354d972732891bb1a94bf225d8769
ArianeKraus <ariane.kraus@hotmail.de>
2020-01-13 11:10:50 +0100

    changes settingsView

--- components/inspectit-ocelot-configurationserver-ui/src/components/views/userSettings/UserSettingsView.js
+++ components/inspectit-ocelot-configurationserver-ui/src/components/views/userSettings/UserSettingsView.js
@@ -2,25 +2,25 @@ import React from 'react';
 import UserSettingsMenu from './UserSettingsMenu';
 
 /**
- * The User Settings View Wrapper, includes the Tab Menu
+ * The User Settings View Wrapper, includes the Tab Menu.
  */
 const UserSettingsView = (props) => {
-  return (
-    <div>
-      <style jsx>{`
-        .content {
-          margin-top: 3rem;
-          overflow: auto auto;
-        }
-      `}</style>
+    return (
+        <div>
+            <style jsx>{`
+                .content {
+                margin-top: 3rem;
+                overflow: auto auto;
+                }
+            `}</style>
 
-      <UserSettingsMenu />
+            <UserSettingsMenu />
 
-      <div className='content'>
-        {props.children}
-      </div>
-    </div>
-  )
+            <div className='content'>
+                {props.children}
+            </div>
+        </div>
+    )
 }
 
-export default UserSettingsView
+export default UserSettingsView;
