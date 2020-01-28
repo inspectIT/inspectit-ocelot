432ac0504282ed62366e988826c8516c4be4b0d1
Ariane Kraus <37696717+ArianeKraus@users.noreply.github.com>
2020-01-28 14:57:07 +0100

    Closes #534 - List the default configuration files in the UI FileTree component (#543)

--- components/inspectit-ocelot-configurationserver-ui/src/components/views/configuration/FileTree.js
+++ components/inspectit-ocelot-configurationserver-ui/src/components/views/configuration/FileTree.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Tree } from 'primereact/tree';
 import { connect } from 'react-redux'
 import { configurationActions, configurationSelectors, configurationUtils } from '../../../redux/ducks/configuration'
+import { linkPrefix } from '../../../lib/configuration';
 
 /**
  * The file tree used in the configuration view.
@@ -26,42 +27,74 @@ class FileTree extends React.Component {
      * Handle tree selection changes.
      */
     onSelectionChange = (event) => {
-        const { selection, rawFiles } = this.props;
-
-        if (event.value !== selection) {
-            this.props.selectFile(event.value);
-        }
+        const { selection,selectedDefaultConfigFile, rawFiles } = this.props;
+        const newSelection = event.value;
+        if(newSelection) {
+            if (newSelection !== selection && newSelection !== selectedDefaultConfigFile) {
+                this.props.selectFile(newSelection);
+            }
+        } else {
+            if (selection || selectedDefaultConfigFile) {
+                this.props.selectFile(null);
+            }
+        }        
     }
 
     render() {
         return (
-            <Tree
-                className={this.props.className}
-                filter={true}
-                filterBy="label"
-                value={this.props.files}
-                selectionMode="single"
-                selectionKeys={this.props.selection}
-                onSelectionChange={this.onSelectionChange}
-            />
+            <div className='this'>
+                <style jsx>{`
+                    .this {
+                        overflow: auto;
+                        flex-grow: 1;
+                    }
+                    .this :global(.cm-tree-icon) {
+                        width: 1.3rem;
+                        height: 1.3rem;
+                    }
+                    .this :global(.cm-tree-label) {
+                        color: #aaa;
+                    }
+                    .this :global(.ocelot-tree-head-orange) {
+                        background: url("${linkPrefix}/static/images/inspectit-ocelot-head_orange.svg") center no-repeat;
+                        background-size: 1rem 1rem;
+                    }
+                    .this :global(.ocelot-tree-head-white) {
+                        background: url("${linkPrefix}/static/images/inspectit-ocelot-head_white.svg") center no-repeat;
+                        background-size: 1rem 1rem;
+                    }
+				`}</style>
+                <Tree
+                    className={this.props.className}
+                    filter={true}
+                    filterBy="label"
+                    value={this.props.defaultTree.concat(this.props.files)}
+                    selectionMode="single"
+                    selectionKeys={this.props.selection || this.props.selectedDefaultConfigFile}
+                    onSelectionChange={this.onSelectionChange}
+                />
+            </div>
+
         );
     }
 }
 
 function mapStateToProps(state) {
-    const { pendingRequests, selection, files, defaultConfig } = state.configuration;
+    const { pendingRequests, selection, files, defaultConfig, selectedDefaultConfigFile } = state.configuration;
     return {
         files: configurationSelectors.getFileTree(state),
         loading: pendingRequests > 0,
         selection,
-        defaultConfig
+        defaultConfig: defaultConfig,
+        defaultTree: configurationSelectors.getDefaultConfigTree(state),
+        selectedDefaultConfigFile
     }
 }
 
 const mapDispatchToProps = {
     fetchDefaultConfig: configurationActions.fetchDefaultConfig,
     fetchFiles: configurationActions.fetchFiles,
-    selectFile: configurationActions.selectFile
+    selectFile: configurationActions.selectFile,
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(FileTree);
