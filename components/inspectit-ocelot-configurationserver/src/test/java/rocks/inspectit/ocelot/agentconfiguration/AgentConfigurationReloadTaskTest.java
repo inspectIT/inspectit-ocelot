b83f26fa835637e27c9c91cfcb483ef75a6787b2
Marius Oehler <marius.oehler@novatec-gmbh.de>
2020-02-28 15:19:38 +0100

    Invalid configuration files only affects the related agent mappings.

--- components/inspectit-ocelot-configurationserver/src/test/java/rocks/inspectit/ocelot/agentconfiguration/AgentConfigurationReloadTaskTest.java
+++ components/inspectit-ocelot-configurationserver/src/test/java/rocks/inspectit/ocelot/agentconfiguration/AgentConfigurationReloadTaskTest.java
@@ -1,5 +1,6 @@
 package rocks.inspectit.ocelot.agentconfiguration;
 
+import org.apache.commons.lang3.mutable.MutableObject;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -12,8 +13,13 @@ import rocks.inspectit.ocelot.mappings.model.AgentMapping;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.function.Consumer;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.*;
 
@@ -25,6 +31,79 @@ public class AgentConfigurationReloadTaskTest {
 
     @Mock
     FileManager fileManager;
+
+    @Nested
+    class Run {
+
+        @Test
+        public void loadWithException() throws Exception {
+            FileInfo fileInfo = mock(FileInfo.class);
+            when(fileInfo.getAbsoluteFilePaths(any())).thenReturn(Stream.of("/test.yml"), Stream.of("/test.yml"));
+            when(fileManager.exists(anyString())).thenReturn(true);
+            when(fileManager.isDirectory(anyString())).thenReturn(true);
+            when(fileManager.getFilesInDirectory(anyString(), anyBoolean())).thenReturn(Arrays.asList(fileInfo));
+            // the first call will return a broken file
+            when(fileManager.readFile(anyString())).thenReturn("key:\tbroken", "key: valid");
+
+            AgentMapping mapping = AgentMapping.builder().name("test").source("/test").build();
+            AgentMapping mapping2 = AgentMapping.builder().name("test2").source("/test2").build();
+
+            final MutableObject<List<AgentConfiguration>> configurations = new MutableObject<>();
+            final CountDownLatch latch = new CountDownLatch(1);
+            Consumer<List<AgentConfiguration>> consumer = (newConfigurations) -> {
+                configurations.setValue(newConfigurations);
+                latch.countDown();
+            };
+
+            AgentConfigurationReloadTask task = new AgentConfigurationReloadTask(Arrays.asList(mapping, mapping2), fileManager, consumer);
+
+            task.run();
+
+            latch.await();
+
+            List<AgentConfiguration> configurationList = configurations.getValue();
+            assertThat(configurationList).hasSize(1);
+            assertThat(configurationList)
+                    .element(0)
+                    .extracting(AgentConfiguration::getConfigYaml)
+                    .isEqualTo("{key: valid}\n");
+        }
+    }
+
+    @Nested
+    class LoadAndMergeYaml {
+
+        @Test
+        public void loadYaml() throws IOException {
+            FileInfo fileInfo = mock(FileInfo.class);
+            when(fileInfo.getAbsoluteFilePaths(any())).thenReturn(Stream.of("/test.yml"));
+            when(fileManager.exists("test")).thenReturn(true);
+            when(fileManager.isDirectory("test")).thenReturn(true);
+            when(fileManager.getFilesInDirectory(anyString(), anyBoolean())).thenReturn(Arrays.asList(fileInfo));
+            when(fileManager.readFile("/test.yml")).thenReturn("key: value");
+
+            AgentMapping mapping = AgentMapping.builder().name("test").source("/test").build();
+            String string = reloadTask.loadConfigForMapping(mapping);
+
+            assertThat(string).isEqualTo("{key: value}\n");
+        }
+
+        @Test
+        public void yamlWithTab() throws IOException {
+            FileInfo fileInfo = mock(FileInfo.class);
+            when(fileInfo.getAbsoluteFilePaths(any())).thenReturn(Stream.of("/test.yml"));
+            when(fileManager.exists("test")).thenReturn(true);
+            when(fileManager.isDirectory("test")).thenReturn(true);
+            when(fileManager.getFilesInDirectory(anyString(), anyBoolean())).thenReturn(Arrays.asList(fileInfo));
+            when(fileManager.readFile("/test.yml")).thenReturn("key:\tvalue");
+
+            AgentMapping mapping = AgentMapping.builder().name("test").source("/test").build();
+
+            assertThatExceptionOfType(AgentConfigurationReloadTask.InvalidConfigurationFileException.class)
+                    .isThrownBy(() -> reloadTask.loadConfigForMapping(mapping))
+                    .withMessage("The configuration file '/test.yml' is invalid and cannot be parsed.");
+        }
+    }
 
     @Nested
     class LoadConfigForMapping {
