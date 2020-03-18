936738d131a409e7e37a09d854251621f7c4d4a6
Marius Brill <53812669+mbrill-nt@users.noreply.github.com>
2020-03-18 08:52:58 +0100

    Closes #314 - Implemented ASM-based solution for plugin scanning. (#557)

---
+++ inspectit-ocelot-core/src/main/java/rocks/inspectit/ocelot/core/plugins/OcelotPluginAnnotationVisitor.java
@@ -0,0 +1,33 @@
+package rocks.inspectit.ocelot.core.plugins;
+
+import org.springframework.asm.AnnotationVisitor;
+import org.springframework.asm.ClassVisitor;
+import org.springframework.asm.Opcodes;
+import org.springframework.asm.Type;
+import rocks.inspectit.ocelot.sdk.OcelotPlugin;
+
+class OcelotPluginAnnotationVisitor extends ClassVisitor {
+    public static final String OCELOT_PLUGIN_DESCRIPTOR = "L" + Type.getInternalName(OcelotPlugin.class) + ";";
+    private boolean hasOcelotPluginAnnotation;
+
+    /**
+     * Checks if a currently visited class annotated with {@link OcelotPlugin}.
+     *
+     * @return Returns true if the currently visited class annotated with {@link OcelotPlugin}.
+     */
+    public boolean hasOcelotPluginAnnotation() {
+        return hasOcelotPluginAnnotation;
+    }
+
+    OcelotPluginAnnotationVisitor() {
+        super(Opcodes.ASM7);
+    }
+
+    @Override
+    public AnnotationVisitor visitAnnotation(String desc, boolean visible) {
+        if (desc.equals(OCELOT_PLUGIN_DESCRIPTOR)) {
+            hasOcelotPluginAnnotation = true;
+        }
+        return null;
+    }
+}
