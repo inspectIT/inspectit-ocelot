b6fbc9b6bc44f947b4a52f17d0da82e18eadec86
Ivan Senic <senic.ivan@gmail.com>
2020-04-07 11:53:30 +0200

    Closes #637 - Exposing count of reported resource timing elements (#651)

--- components/inspectit-ocelot-eum-server/src/main/java/rocks/inspectit/oce/eum/server/metrics/BeaconMetricManager.java
+++ components/inspectit-ocelot-eum-server/src/main/java/rocks/inspectit/oce/eum/server/metrics/BeaconMetricManager.java
@@ -7,13 +7,16 @@ import io.opencensus.tags.TagValue;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
 import rocks.inspectit.oce.eum.server.arithmetic.RawExpression;
 import rocks.inspectit.oce.eum.server.beacon.Beacon;
+import rocks.inspectit.oce.eum.server.beacon.recorder.BeaconRecorder;
 import rocks.inspectit.oce.eum.server.configuration.model.BeaconMetricDefinitionSettings;
 import rocks.inspectit.oce.eum.server.configuration.model.BeaconRequirement;
 import rocks.inspectit.oce.eum.server.configuration.model.EumServerConfiguration;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -28,6 +31,9 @@ public class BeaconMetricManager {
 
     @Autowired
     private MeasuresAndViewsManager measuresAndViewsManager;
+
+    @Autowired(required = false)
+    private List<BeaconRecorder> beaconRecorders;
 
     /**
      * Maps metric definitions to expressions.
@@ -52,6 +58,13 @@ public class BeaconMetricManager {
                 successful = true;
             } else {
                 log.debug("Skipping beacon because requirements are not fulfilled.");
+            }
+        }
+
+        // allow each beacon recorder to record stuff
+        if (!CollectionUtils.isEmpty(beaconRecorders)) {
+            try (Scope scope = getTagContextForBeacon(beacon).buildScoped()) {
+                beaconRecorders.forEach(beaconRecorder -> beaconRecorder.record(beacon));
             }
         }
 
