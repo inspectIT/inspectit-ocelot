25e39f9f3e5652d868ffa8b9a72023c5dd1ebd58
Ivan Senic <senic.ivan@gmail.com>
2020-05-07 10:58:18 +0200

    integration changes #2

--- components/inspectit-ocelot-eum-server/src/main/java/rocks/inspectit/oce/eum/server/exporters/configuration/TraceExportersConfiguration.java
+++ components/inspectit-ocelot-eum-server/src/main/java/rocks/inspectit/oce/eum/server/exporters/configuration/TraceExportersConfiguration.java
@@ -2,7 +2,6 @@ package rocks.inspectit.oce.eum.server.exporters.configuration;
 
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
-import io.micrometer.core.instrument.util.StringUtils;
 import io.opentelemetry.exporters.jaeger.JaegerGrpcSpanExporter;
 import io.opentelemetry.sdk.trace.export.SpanExporter;
 import lombok.extern.slf4j.Slf4j;
@@ -11,6 +10,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StringUtils;
 import rocks.inspectit.oce.eum.server.configuration.model.EumServerConfiguration;
 import rocks.inspectit.ocelot.config.model.exporters.ExportersSettings;
 import rocks.inspectit.ocelot.config.model.exporters.trace.JaegerExporterSettings;
@@ -33,7 +33,7 @@ public class TraceExportersConfiguration {
                 .map(TraceExportersSettings::getJaeger)
                 .filter(JaegerExporterSettings::isEnabled)
                 .ifPresent(settings -> {
-                    if (StringUtils.isNotBlank(settings.getUrl()) && StringUtils.isBlank(settings.getGrpc())) {
+                    if (!StringUtils.isEmpty(settings.getUrl()) && StringUtils.isEmpty(settings.getGrpc())) {
                         log.warn("In order to use Jaeger span exporter, please specify the grpc API endpoint property instead of the url.");
                     }
                 });
