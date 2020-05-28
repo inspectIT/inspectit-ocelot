2535738fc33a1d643c8706f38b577151e225cee2
Marius Oehler <marius.oehler@novatec-gmbh.de>
2020-05-28 08:20:15 +0200

    Fixed regex matching

--- components/inspectit-ocelot-configurationserver/src/main/java/rocks/inspectit/ocelot/mappings/model/AgentMapping.java
+++ components/inspectit-ocelot-configurationserver/src/main/java/rocks/inspectit/ocelot/mappings/model/AgentMapping.java
@@ -14,6 +14,7 @@ import javax.validation.constraints.NotBlank;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Pattern;
 
 /**
  * The model of the agent mappings.
@@ -55,10 +56,13 @@ public class AgentMapping implements Auditable {
      */
     public boolean matchesAttributes(Map<String, String> agentAttributes) {
         for (Map.Entry<String, String> pair : attributes.entrySet()) {
-            String expected = pair.getValue();
+            String pattern = pair.getValue();
             String value = agentAttributes.getOrDefault(pair.getKey(), "");
 
-            if (!StringUtils.equalsIgnoreCase(value, expected)) {
+            boolean matches = Pattern.compile(pattern, Pattern.CASE_INSENSITIVE)
+                    .matcher(value)
+                    .matches();
+            if (!matches) {
                 return false;
             }
         }
--- components/inspectit-ocelot-configurationserver/src/test/java/rocks/inspectit/ocelot/mappings/model/AgentMappingTest.java
+++ components/inspectit-ocelot-configurationserver/src/test/java/rocks/inspectit/ocelot/mappings/model/AgentMappingTest.java
@@ -21,6 +21,7 @@ class AgentMappingTest {
         Map<String, String> attributes = new HashMap<>();
         attributes.put("first", "01234");
         attributes.put("second", "AnAttribute");
+        attributes.put("third", "\\d+");
         List<String> sources = new ArrayList<>();
         sources.add("/one");
         sources.add("/two/a.yml");
@@ -33,7 +34,7 @@ class AgentMappingTest {
 
         @Test
         public void sameAttributes() {
-            Map<String, String> attributes = ImmutableMap.of("first", "01234", "second", "AnAttribute");
+            Map<String, String> attributes = ImmutableMap.of("first", "01234", "second", "AnAttribute", "third", "0");
 
             boolean result = mapping.matchesAttributes(attributes);
 
@@ -42,7 +43,7 @@ class AgentMappingTest {
 
         @Test
         public void differentCasing() {
-            Map<String, String> attributes = ImmutableMap.of("first", "01234", "second", "anaTTribute");
+            Map<String, String> attributes = ImmutableMap.of("first", "01234", "second", "anaTTribute", "third", "0");
 
             boolean result = mapping.matchesAttributes(attributes);
 
@@ -51,7 +52,7 @@ class AgentMappingTest {
 
         @Test
         public void differentAttributes() {
-            Map<String, String> attributes = ImmutableMap.of("first", "00000", "second", "AnAttribute");
+            Map<String, String> attributes = ImmutableMap.of("first", "00000", "second", "AnAttribute", "third", "0");
 
             boolean result = mapping.matchesAttributes(attributes);
 
@@ -67,6 +68,14 @@ class AgentMappingTest {
             assertThat(result).isFalse();
         }
 
+        @Test
+        public void wrongRegexAttribute() {
+            Map<String, String> attributes = ImmutableMap.of("first", "01234", "second", "AnAttribute", "third", "");
+
+            boolean result = mapping.matchesAttributes(attributes);
+
+            assertThat(result).isFalse();
+        }
     }
 
 }
