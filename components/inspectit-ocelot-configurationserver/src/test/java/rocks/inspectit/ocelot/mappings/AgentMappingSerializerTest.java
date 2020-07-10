69aac0e960d5df169b617d4a9e6d6b522c61676b
Marius Oehler <marius.oehler@novatec-gmbh.de>
2020-07-10 12:04:33 +0200

    fixed bug in agent serializer

--- components/inspectit-ocelot-configurationserver/src/test/java/rocks/inspectit/ocelot/mappings/AgentMappingSerializerTest.java
+++ components/inspectit-ocelot-configurationserver/src/test/java/rocks/inspectit/ocelot/mappings/AgentMappingSerializerTest.java
@@ -51,11 +51,11 @@ public class AgentMappingSerializerTest {
             verify(fileAccessor).writeAgentMappings(writtenFile.capture());
 
             assertThat(writtenFile.getValue()).isEqualTo("- name: \"mapping\"\n" +
+                    "  sourceBranch: \"LIVE\"\n"+
                     "  sources:\n" +
                     "  - \"/any-source\"\n" +
                     "  attributes:\n" +
-                    "    key: \"val\"\n" +
-                    "  sourceBranch: \"LIVE\"\n");
+                    "    key: \"val\"\n");
             verifyNoMoreInteractions(fileAccessor);
         }
     }
@@ -64,9 +64,9 @@ public class AgentMappingSerializerTest {
     public class ReadAgentMappings {
 
         @Test
-        public void successfullyReadYaml() throws IOException {
+        public void successfullyReadYaml() {
             String dummyYaml = "- name: \"mapping\"\n" +
-                    "  branch: \"WORKSPACE\"\n" +
+                    "  sourceBranch: \"LIVE\"\n" +
                     "  sources:\n" +
                     "  - \"/any-source\"\n" +
                     "  attributes:\n" +
@@ -81,6 +81,7 @@ public class AgentMappingSerializerTest {
             assertThat(mapping.getName()).isEqualTo("mapping");
             assertThat(mapping.getSources()).containsExactly("/any-source");
             assertThat(mapping.getAttributes()).containsEntry("key", "val");
+            assertThat(mapping.getSourceBranch()).isEqualTo(Branch.LIVE);
         }
     }
 }
