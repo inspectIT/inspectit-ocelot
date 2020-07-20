1f565a6ebb5c2b9d8bca47a3f59ecad66f535e05
ArianeKraus <ariane.kraus@hotmail.de>
2020-07-20 18:21:10 +0200

    added eventexportservice, resolving and exporting events towards service

---
+++ inspectit-ocelot-core/src/main/java/rocks/inspectit/ocelot/core/exporter/EventExporterService.java
@@ -0,0 +1,103 @@
+package rocks.inspectit.ocelot.core.exporter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import rocks.inspectit.ocelot.config.model.InspectitConfig;
+import rocks.inspectit.ocelot.config.model.events.EventSettings;
+import rocks.inspectit.ocelot.core.service.DynamicallyActivatableService;
+import rocks.inspectit.ocelot.sdk.events.Event;
+
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
+@Component
+@Slf4j
+public class EventExporterService  extends DynamicallyActivatableService {
+
+    /** The queue which stores the event objects to be send. */
+    private static ArrayList<Event> eventQueue = new ArrayList<Event>();
+
+    /** The scheduler for periodically send the stored events within eventQueue towards registered exporters. */
+    @Autowired
+    private ScheduledExecutorService executor;
+
+    /** The future for the scheduled task. Will be closed during doDisable instead of calling executor.shudown(). */
+    private ScheduledFuture<?> schedule;
+
+    public EventExporterService() {
+        super("events");
+    }
+
+    /**
+     * Function called by {EventRecorder} in order to send Events.
+     * @param eventObj - An Event which should be send towards exporters.
+     */
+    public void export(Event eventObj) {
+        if(eventObj != null && !schedule.isCancelled()) {
+            eventQueue.add(eventObj);
+        }
+    }
+
+    private void sendToHandlers() {
+        if(eventQueue.isEmpty()) {
+            return;
+        }
+        // Call to exporting events
+        eventQueue.clear();
+    }
+
+    @Override
+    protected boolean checkEnabledForConfig(InspectitConfig configuration) {
+        @Valid EventSettings settings = configuration.getEvents();
+        return settings.isEnabled()
+                && settings.getFrequency() != null;
+    }
+
+    @Override
+    protected boolean doEnable(InspectitConfig configuration) {
+        log.info("Starting EventExporter Service");
+        try {
+            EventSettings settings = configuration.getEvents();
+            /**
+             * Alternative:
+             * if(executor not undefined){
+             *     executor = new ScheduledExecutorService();
+             *     executor.scheduleAtFixesRate(....);
+             * }
+             */
+            schedule = executor.scheduleAtFixedRate(
+                    () -> sendToHandlers(),
+                    settings.getFrequency().getSeconds(),
+                    settings.getFrequency().getSeconds(),
+                    TimeUnit.SECONDS
+            );
+        } catch (Throwable t) {
+            log.error("EventExporter Service could not be started.", t);
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    protected boolean doDisable() {
+        log.info("Shutting down EventExporter Service.");
+
+        if(schedule.cancel(false)) {
+            return true;
+        }
+
+        /**
+         * Alternative:
+         * try{
+         *     executor.shutdown()
+         * }
+         */
+
+        log.error("Shutting down EventExporter Service failed.");
+        return false;
+    }
+}
--- inspectit-ocelot-core/src/main/java/rocks/inspectit/ocelot/core/instrumentation/hook/actions/EventRecorder.java
+++ inspectit-ocelot-core/src/main/java/rocks/inspectit/ocelot/core/instrumentation/hook/actions/EventRecorder.java
@@ -1,7 +1,10 @@
 package rocks.inspectit.ocelot.core.instrumentation.hook.actions;
 
 import lombok.Value;
+import rocks.inspectit.ocelot.core.exporter.EventExporterService;
+import rocks.inspectit.ocelot.core.instrumentation.hook.VariableAccessor;
 import rocks.inspectit.ocelot.core.instrumentation.hook.actions.model.EventAccessor;
+import rocks.inspectit.ocelot.sdk.events.Event;
 
 import java.util.*;
 
@@ -10,16 +13,76 @@ public class EventRecorder implements IHookAction{
 
     List<EventAccessor> eventAccessors;
 
-    @Override
-    public void execute(IHookAction.ExecutionContext context) {
-        for(EventAccessor event : eventAccessors) {
-            // Do nothing for now
-        }
-    }
+    EventExporterService eventExporterService;
 
     @Override
     public String getName() {
         return "Event Recorder";
+    }
+
+    @Override
+    public void execute(IHookAction.ExecutionContext context) {
+        for(EventAccessor event : eventAccessors) {
+            Event res = new Event();
+            res.setName(resolveEventName(event, context));
+            res.setTimestamp(System.currentTimeMillis());
+
+            HashMap<String, Object> resolvedAtts = new HashMap<>();
+            resolveMap(resolvedAtts, event.getAttributes(), event, context);
+            res.setAttributes(resolvedAtts);
+
+            eventExporterService.export(res);
+        }
+    }
+
+    private String resolveEventName(EventAccessor eventAccessor, ExecutionContext context) {
+        String dataKey = eventAccessor.getName();
+
+        try {
+            VariableAccessor valueAccessor = eventAccessor.getVariableAccessors().get(dataKey);
+            Object value = valueAccessor.get(context);
+            if(value != null) {
+                return (String) value;
+            }
+            return dataKey;
+        } catch (Throwable t) {
+            if (eventAccessor.getConstantTags().containsKey(dataKey)) {
+                return eventAccessor.getConstantTags().get(dataKey);
+            }
+            return dataKey;
+        }
+    }
+
+    private void resolveMap(Map<String, Object> copy, Map<String, Object> origin, EventAccessor eventAccessor, ExecutionContext context) {
+        for (Map.Entry<String, Object> entry : origin.entrySet()) {
+            Object content = entry.getValue();
+            if (content instanceof Map) {
+                HashMap<String, Object> copyContent = new HashMap<>();
+                copy.put(entry.getKey(), copyContent);
+
+                Map<String, Object> contentAsMap = (Map) content;
+                resolveMap(copyContent, contentAsMap, eventAccessor, context);
+            } else {
+                Object value = resolveDataKey((String) content, eventAccessor, context);
+                if (value != null) {
+                    copy.put(entry.getKey(), value);
+                }
+            }
+        }
+    }
+
+    private Object resolveDataKey(String key, EventAccessor eventAccessor, ExecutionContext context) {
+        if(eventAccessor.getVariableAccessors().containsKey(key)) {
+            try {
+                VariableAccessor valueAccessor = eventAccessor.getVariableAccessors().get(key);
+                return valueAccessor.get(context);
+            } catch(Throwable t){
+                //ignore
+            }
+        } else if(eventAccessor.getConstantTags().containsKey(key)) {
+            return eventAccessor.getConstantTags().get(key);
+        }
+        return null;
     }
 
 }
