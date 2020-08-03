5d247067ed599fec18f7d2b51d739c40f8d6d153
Jonas Kunz <Jonas.Kunz@gmx.net>
2020-08-03 11:04:40 +0200

    Removed debugging stuff

--- components/inspectit-ocelot-eum-server/src/test/java/rocks/inspectit/oce/eum/server/exporters/PrometheusExporterServiceIntTest.java
+++ components/inspectit-ocelot-eum-server/src/test/java/rocks/inspectit/oce/eum/server/exporters/PrometheusExporterServiceIntTest.java
@@ -108,12 +108,6 @@ public class PrometheusExporterServiceIntTest {
         Map<String, String> beacon = getBasicBeacon();
         beacon.put(FAKE_BEACON_KEY_NAME, "Fake Value");
 
-        await().atMost(15, TimeUnit.SECONDS).untilAsserted(() -> {
-            HttpResponse response = testClient.execute(new HttpGet("http://localhost:8888/metrics)"));
-            ResponseHandler responseHandler = new BasicResponseHandler();
-            assertThat(responseHandler.handleResponse(response).toString()).isNotEmpty();
-        });
-
         sendBeacon(beacon);
 
         await().atMost(15, TimeUnit.SECONDS).untilAsserted(() -> {
@@ -133,13 +127,6 @@ public class PrometheusExporterServiceIntTest {
     public void expectOneView() throws Exception {
         Map<String, String> beacon = getBasicBeacon();
         beacon.put(BEACON_KEY_NAME, "12");
-
-        await().atMost(15, TimeUnit.SECONDS).untilAsserted(() -> {
-            assertGet200("http://localhost:8888/metrics");
-            HttpResponse response = testClient.execute(new HttpGet("http://localhost:8888/metrics)"));
-            ResponseHandler responseHandler = new BasicResponseHandler();
-            assertThat(responseHandler.handleResponse(response).toString()).isNotEmpty();
-        });
 
         sendBeacon(beacon);
 
