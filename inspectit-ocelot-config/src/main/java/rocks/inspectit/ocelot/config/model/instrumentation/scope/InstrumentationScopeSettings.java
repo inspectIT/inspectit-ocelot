47b05cd2e0f5863fe306841f07d98e9e2ca06652
Patrick <patrick.eichhorn@novatec-gmbh.de>
2020-10-19 13:53:47 +0200

    Exclude Scopes

--- inspectit-ocelot-config/src/main/java/rocks/inspectit/ocelot/config/model/instrumentation/scope/InstrumentationScopeSettings.java
+++ inspectit-ocelot-config/src/main/java/rocks/inspectit/ocelot/config/model/instrumentation/scope/InstrumentationScopeSettings.java
@@ -4,12 +4,14 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import org.springframework.util.CollectionUtils;
+import rocks.inspectit.ocelot.config.model.InspectitConfig;
+import rocks.inspectit.ocelot.config.model.instrumentation.InstrumentationSettings;
+import rocks.inspectit.ocelot.config.validation.ViolationBuilder;
 
 import javax.validation.Valid;
 import javax.validation.constraints.AssertTrue;
 import javax.validation.constraints.NotNull;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 /**
  * Data container for settings which are used as basis for {@link rocks.inspectit.ocelot.core.instrumentation.config.model.InstrumentationScope}.
@@ -43,6 +45,12 @@ public class InstrumentationScopeSettings {
     private ElementDescriptionMatcherSettings type;
 
     /**
+     * Scopes which are to be excluded
+     */
+    @Valid
+    private Map<String, Boolean> exclude = Collections.emptyMap();
+
+    /**
      * Defines which methods are targeted by this scope.
      */
     @Valid
@@ -68,5 +76,64 @@ public class InstrumentationScopeSettings {
         return !CollectionUtils.isEmpty(interfaces)
                 || superclass != null
                 || (type != null && !type.isAnyMatcher());
+    }
+
+    /**
+     * Validates this scope, invoked by {@link InstrumentationSettings#performValidation(InspectitConfig, ViolationBuilder)}
+     *
+     * @param container      the root config containing this rule
+     * @param vios           the violation builder
+     */
+    public void performValidation(String name, InstrumentationSettings container, ViolationBuilder vios, Set<String> verified) {
+        // Verify that the excluded scopes have also been defined.
+        exclude.entrySet().stream()
+                .filter(Map.Entry::getValue)
+                .map(Map.Entry::getKey)
+                .filter(excludeName -> !container.getScopes().containsKey(excludeName))
+                .forEach(excludeName -> vios.message("Specified excluded scope '{scope}' was not defined!")
+                        .atProperty("scopes")
+                        .parameter("scope", excludeName)
+                        .buildAndPublish());
+
+        ArrayList<String> visitedParents = new ArrayList<>();
+        visitedParents.add(name);
+        verifyCyclicalDependence(name, container.getScopes(), verified, visitedParents, vios);
+
+    }
+
+    /**
+     * Verify that there are no cyclic dependencies between the excluded scopes.
+     *
+     * @param parentScope the scope, which holds the exclude-scopes
+     * @param scopes map of scopes
+     * @param verified a list containing already verified scopes
+     * @param visitedParents temp list for verified parentScopes
+     * @param vios the violation output
+     */
+    private static void verifyCyclicalDependence(String parentScope, Map<String, InstrumentationScopeSettings> scopes, Set<String> verified, List<String> visitedParents, ViolationBuilder vios) {
+        if(verified.contains(parentScope)){
+            return;
+        }
+
+        scopes.get(parentScope).getExclude().entrySet().stream()
+                .filter(Map.Entry::getValue)
+                .map(Map.Entry::getKey)
+                .forEach(excludeScope -> {
+                    if (visitedParents.contains(excludeScope)) {
+                        vios.message("Specified excluded Scope in '{scope}' has cyclical dependence with other Scope -> '{excludeScope}'") // verifiedParens ausgeben
+                                .atProperty("scopes")
+                                .parameter("scope", parentScope)
+                                .parameter("excludeScope", excludeScope)
+                                .buildAndPublish();
+                    }
+                    else {
+                        visitedParents.add(excludeScope);
+                        if (scopes.containsKey(excludeScope)) {
+                            verifyCyclicalDependence(excludeScope, scopes, verified, visitedParents, vios);
+                        }
+                        visitedParents.remove(visitedParents.size() - 1);
+                    }
+                });
+        verified.add(parentScope);
     }
 }
