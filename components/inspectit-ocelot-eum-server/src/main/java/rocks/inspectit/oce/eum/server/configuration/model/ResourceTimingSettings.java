aedce904f1fd920bcd0ceb3c0adc69ccd2eaab52
Patrick <patrick.eichhorn@novatec-gmbh.de>
2021-02-02 10:58:50 +0100

    added 'tags' property

--- components/inspectit-ocelot-eum-server/src/main/java/rocks/inspectit/oce/eum/server/configuration/model/ResourceTimingSettings.java
+++ components/inspectit-ocelot-eum-server/src/main/java/rocks/inspectit/oce/eum/server/configuration/model/ResourceTimingSettings.java
@@ -1,7 +1,12 @@
 package rocks.inspectit.oce.eum.server.configuration.model;
 
 import lombok.Data;
+import lombok.Singular;
 import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.util.Map;
 
 /**
  * Resource timing settings.
@@ -14,5 +19,8 @@ public class ResourceTimingSettings {
      * If resource timing is enabled or not.
      */
     private boolean enabled;
+
+    @Singular
+    private Map<@NotBlank String, @NotNull Boolean> tags;
 
 }
