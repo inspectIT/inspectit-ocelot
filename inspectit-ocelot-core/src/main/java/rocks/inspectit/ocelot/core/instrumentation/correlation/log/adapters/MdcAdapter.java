11e6e2a2a15c561d515247a07a92c6db17a8b498
Marius Oehler <marius.oehler@novatec-gmbh.de>
2021-02-17 11:24:09 +0100

    removed unused classes and tests

--- inspectit-ocelot-core/src/main/java/rocks/inspectit/ocelot/core/instrumentation/correlation/log/adapters/MdcAdapter.java
+++ inspectit-ocelot-core/src/main/java/rocks/inspectit/ocelot/core/instrumentation/correlation/log/adapters/MdcAdapter.java
@@ -10,17 +10,6 @@ import java.lang.reflect.Method;
  */
 public interface MdcAdapter {
 
-//    /**
-//     * Writes a given entry to the MDC.
-//     * If the value is null, the entry is removed from the MDC.
-//     *
-//     * @param key   the MDC key to use
-//     * @param value the value to place in the MDC, null if the value for the key should be erased
-//     *
-//     * @return a {@link Undo} which reverts the change performed by this method call.
-//     */
-//    MDCAccess.Undo set(String key, String value);
-
     Method getGetMethod(Class<?> mdcClazz) throws NoSuchMethodException;
 
     Method getPutMethod(Class<?> mdcClazz) throws NoSuchMethodException;
@@ -29,12 +18,4 @@ public interface MdcAdapter {
 
     DelegationMdcAccessor wrap(MdcAccessor mdcAccessor);
 
-    /**
-     * Checks if this Adapter is enabled based on the given configuration.
-     *
-     * @param settings the currently active configuration
-     *
-     * @return true if enabled, false otherwise
-     */
-//    boolean isEnabledForConfig(TraceIdMDCInjectionSettings settings);
 }
