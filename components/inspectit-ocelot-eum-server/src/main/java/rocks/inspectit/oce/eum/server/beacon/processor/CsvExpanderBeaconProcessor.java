1bd3ed4df1d7bd101f4099dc7d7d80a58fc81bb9
Patrick <patrick.eichhorn@novatec-gmbh.de>
2021-03-03 15:11:50 +0100

    remove rt.bmr.sum and change value index logic

--- components/inspectit-ocelot-eum-server/src/main/java/rocks/inspectit/oce/eum/server/beacon/processor/CsvExpanderBeaconProcessor.java
+++ components/inspectit-ocelot-eum-server/src/main/java/rocks/inspectit/oce/eum/server/beacon/processor/CsvExpanderBeaconProcessor.java
@@ -1,7 +1,10 @@
 package rocks.inspectit.oce.eum.server.beacon.processor;
 
+import com.google.common.annotations.VisibleForTesting;
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.math.NumberUtils;
 import org.springframework.stereotype.Component;
 import rocks.inspectit.oce.eum.server.beacon.Beacon;
 
@@ -12,7 +15,6 @@ import java.util.Collections;
  * indexed key in the beacon.
  * <p>
  * Example: The attribute <code>rt.bmr=37,5</code> will result in <code>rt.bmr.0: 37</code>, <code>rt.bmr.1: 5</code>
- * and <code>rt.bmr.sum: 42</code>
  */
 @Slf4j
 @Component
@@ -21,37 +23,37 @@ public class CsvExpanderBeaconProcessor implements BeaconProcessor {
     /**
      * The target key of the attribute to expand.
      */
-    private static final String ATTRIBUTE_KEY = "rt.bmr";
+    @VisibleForTesting
+    static final String ATTRIBUTE_KEY = "rt.bmr";
 
     /**
      * The regex (character) used to separate individual value groups.
      */
     private static final String GROUP_SEPARATOR = ",";
 
+    /**
+     * List of names for all possible values.
+     */
+    @VisibleForTesting
+    static final String[] VALUE_NAMES = {"startTime", "responseEnd", "responseStart", "requestStart", "connectEnd", "secureConnectionStart", "connectStart", "domainLookupEnd", "domainLookupStart", "redirectEnd", "redirectStart"};
+
     @Override
     public Beacon process(Beacon beacon) {
         String targetAttribute = beacon.get(ATTRIBUTE_KEY);
 
         if (StringUtils.isNoneBlank(targetAttribute)) {
-            int sum = 0;
-            boolean isSum = false;
             String[] attributes = targetAttribute.split(GROUP_SEPARATOR);
 
-            for (int i = 0; i < attributes.length; i++) {
-                String resultKey = ATTRIBUTE_KEY + "." + i;
+            for (int i = 0; i < VALUE_NAMES.length; i++) {
+                String resultKey = ATTRIBUTE_KEY + "." + VALUE_NAMES[i];
 
-                if (!StringUtils.isBlank(attributes[i])) {
-                    try {
-                        sum += Integer.parseInt(attributes[i]);
-                        isSum = true;
-                        beacon = beacon.merge(Collections.singletonMap(resultKey, attributes[i]));
-                    } catch (Exception e) {
-                        log.trace("Error parsing the value <'{}'>: invalid number.", attributes[i]);
-                    }
+                String value = (i < attributes.length && !StringUtils.isBlank(attributes[i])) ? attributes[i] : "0";
+
+                if (NumberUtils.isCreatable(value)) {
+                    beacon = beacon.merge(Collections.singletonMap(resultKey, value));
+                } else {
+                    log.trace("Error parsing the value <'{}'>: invalid number.", attributes[i]);
                 }
-            }
-            if (isSum) {
-                beacon = beacon.merge(Collections.singletonMap(ATTRIBUTE_KEY + ".sum", String.valueOf(sum)));
             }
         }
         return beacon;
--- components/inspectit-ocelot-eum-server/src/test/java/rocks/inspectit/oce/eum/server/beacon/processor/CsvExpanderBeaconProcessorTest.java
+++ components/inspectit-ocelot-eum-server/src/test/java/rocks/inspectit/oce/eum/server/beacon/processor/CsvExpanderBeaconProcessorTest.java
@@ -7,10 +7,10 @@ import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
 import rocks.inspectit.oce.eum.server.beacon.Beacon;
 
+import java.util.Arrays;
 import java.util.Collections;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.entry;
+import static org.assertj.core.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
 class CsvExpanderBeaconProcessorTest {
@@ -20,6 +20,13 @@ class CsvExpanderBeaconProcessorTest {
 
     @Nested
     public class Process {
+
+        private final String[] keyNames = Arrays.stream(CsvExpanderBeaconProcessor.VALUE_NAMES)
+                .sequential()
+                .map(s -> CsvExpanderBeaconProcessor.ATTRIBUTE_KEY + "." + s)
+                .toArray(String[]::new);
+
+        private final Integer maxExpectedSize = keyNames.length + 1;
 
         @Test
         public void noRtBmrAttribute() {
@@ -40,12 +47,14 @@ class CsvExpanderBeaconProcessorTest {
         }
 
         @Test
-        public void rtBmrWithValue() {
+        public void rtBmrWithOneValue() {
             Beacon beacon = Beacon.of(Collections.singletonMap("rt.bmr", "123"));
 
             Beacon result = processor.process(beacon);
 
-            assertThat(result.toMap()).containsOnly(entry("rt.bmr", "123"), entry("rt.bmr.0", "123"), entry("rt.bmr.sum", "123"));
+            assertThat(result.toMap()).containsKeys(keyNames)
+                    .hasSize(maxExpectedSize)
+                    .containsOnly(entry("rt.bmr", "123"), entry("rt.bmr.startTime", "123"), entry("rt.bmr.responseEnd", "0"), entry("rt.bmr.responseStart", "0"), entry("rt.bmr.requestStart", "0"), entry("rt.bmr.connectEnd", "0"), entry("rt.bmr.secureConnectionStart", "0"), entry("rt.bmr.connectStart", "0"), entry("rt.bmr.domainLookupEnd", "0"), entry("rt.bmr.domainLookupStart", "0"), entry("rt.bmr.redirectEnd", "0"), entry("rt.bmr.redirectStart", "0"));
         }
 
         @Test
@@ -54,7 +63,9 @@ class CsvExpanderBeaconProcessorTest {
 
             Beacon result = processor.process(beacon);
 
-            assertThat(result.toMap()).containsOnly(entry("rt.bmr", "foo"));
+            assertThat(result.toMap()).doesNotContainKey("rt.bmr.startTime")
+                    .hasSize(maxExpectedSize - 1)
+                    .contains(entry("rt.bmr", "foo"));
         }
 
         @Test
@@ -63,7 +74,9 @@ class CsvExpanderBeaconProcessorTest {
 
             Beacon result = processor.process(beacon);
 
-            assertThat(result.toMap()).containsOnly(entry("rt.bmr", "0"), entry("rt.bmr.0", "0"), entry("rt.bmr.sum", "0"));
+            assertThat(result.toMap()).containsKeys(keyNames)
+                    .hasSize(maxExpectedSize)
+                    .contains(entry("rt.bmr", "0"), entry("rt.bmr.startTime", "0"));
         }
 
         @Test
@@ -72,7 +85,9 @@ class CsvExpanderBeaconProcessorTest {
 
             Beacon result = processor.process(beacon);
 
-            assertThat(result.toMap()).containsOnly(entry("rt.bmr", "0,0"), entry("rt.bmr.0", "0"), entry("rt.bmr.1", "0"), entry("rt.bmr.sum", "0"));
+            assertThat(result.toMap()).containsKeys(keyNames)
+                    .hasSize(maxExpectedSize)
+                    .contains(entry("rt.bmr", "0,0"), entry("rt.bmr.startTime", "0"), entry("rt.bmr.responseEnd", "0"));
         }
 
         @Test
@@ -81,7 +96,9 @@ class CsvExpanderBeaconProcessorTest {
 
             Beacon result = processor.process(beacon);
 
-            assertThat(result.toMap()).containsOnly(entry("rt.bmr", "123,321"), entry("rt.bmr.0", "123"), entry("rt.bmr.1", "321"), entry("rt.bmr.sum", "444"));
+            assertThat(result.toMap()).containsKeys(keyNames)
+                    .hasSize(maxExpectedSize)
+                    .contains(entry("rt.bmr", "123,321"), entry("rt.bmr.startTime", "123"), entry("rt.bmr.responseEnd", "321"));
         }
 
         @Test
@@ -90,7 +107,9 @@ class CsvExpanderBeaconProcessorTest {
 
             Beacon result = processor.process(beacon);
 
-            assertThat(result.toMap()).containsOnly(entry("rt.bmr", "123,,321"), entry("rt.bmr.0", "123"), entry("rt.bmr.2", "321"), entry("rt.bmr.sum", "444"));
+            assertThat(result.toMap()).containsKeys(keyNames)
+                    .hasSize(maxExpectedSize)
+                    .contains(entry("rt.bmr", "123,,321"), entry("rt.bmr.startTime", "123"), entry("rt.bmr.responseEnd", "0"), entry("rt.bmr.responseStart", "321"));
         }
 
         @Test
@@ -99,7 +118,9 @@ class CsvExpanderBeaconProcessorTest {
 
             Beacon result = processor.process(beacon);
 
-            assertThat(result.toMap()).containsOnly(entry("rt.bmr", "123,bar,321"), entry("rt.bmr.0", "123"), entry("rt.bmr.2", "321"), entry("rt.bmr.sum", "444"));
+            assertThat(result.toMap()).doesNotContainKey("rt.bmr.responseEnd")
+                    .hasSize(maxExpectedSize - 1)
+                    .contains(entry("rt.bmr", "123,bar,321"), entry("rt.bmr.startTime", "123"), entry("rt.bmr.responseStart", "321"));
         }
 
         @Test
@@ -108,7 +129,20 @@ class CsvExpanderBeaconProcessorTest {
 
             Beacon result = processor.process(beacon);
 
-            assertThat(result.toMap()).containsOnly(entry("rt.bmr", "123,bar,321,foo"), entry("rt.bmr.0", "123"), entry("rt.bmr.2", "321"), entry("rt.bmr.sum", "444"));
+            assertThat(result.toMap()).doesNotContainKeys("rt.bmr.responseEnd", "rt.bmr.requestStart")
+                    .hasSize(maxExpectedSize - 2)
+                    .contains(entry("rt.bmr", "123,bar,321,foo"), entry("rt.bmr.startTime", "123"), entry("rt.bmr.responseStart", "321"));
+        }
+
+        @Test
+        public void rtBmrAllValues() {
+            Beacon beacon = Beacon.of(Collections.singletonMap("rt.bmr", "123,120,,,,1,,,7,,,"));
+
+            Beacon result = processor.process(beacon);
+
+            assertThat(result.toMap()).containsKeys(keyNames)
+                    .hasSize(maxExpectedSize)
+                    .contains(entry("rt.bmr", "123,120,,,,1,,,7,,,"), entry("rt.bmr.startTime", "123"), entry("rt.bmr.responseEnd", "120"), entry("rt.bmr.secureConnectionStart", "1"), entry("rt.bmr.domainLookupStart", "7"));
         }
     }
 }
