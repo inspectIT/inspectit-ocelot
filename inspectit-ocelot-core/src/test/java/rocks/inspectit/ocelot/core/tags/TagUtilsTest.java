ad1d50c987da1b0748205cb3501f7b2332c307df
Patrick <patrick.eichhorn@novatec-gmbh.de>
2021-04-30 09:20:21 +0200

    local_test

--- inspectit-ocelot-core/src/test/java/rocks/inspectit/ocelot/core/tags/TagUtilsTest.java
+++ inspectit-ocelot-core/src/test/java/rocks/inspectit/ocelot/core/tags/TagUtilsTest.java
@@ -1,11 +1,13 @@
 package rocks.inspectit.ocelot.core.tags;
 
+import ch.qos.logback.classic.Level;
 import io.opencensus.tags.TagValue;
 import org.junit.jupiter.api.Test;
+import rocks.inspectit.ocelot.core.SpringTestBase;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class TagUtilsTest {
+public class TagUtilsTest extends SpringTestBase {
 
     @Test
     public void createTagValue() {
@@ -16,11 +18,20 @@ public class TagUtilsTest {
     public void createTagValue_tooLong() {
         assertThat(TagUtils.createTagValue("my-tag-key", "this-value-is-over-255-characters-long ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"))
                 .isEqualTo(TagValue.create("<invalid>"));
+        assertLogsOfCount("Error creating value for tag", 1);
     }
 
     @Test
     public void createTagValue_nonPrintableCharacter() {
         assertThat(TagUtils.createTagValue("my-tag-key", "non-printable-character-\u007f")).isEqualTo(TagValue.create("<invalid>"));
+    }
+
+    @Test
+    public void multipleCreateTagValue_nonPrintableCharacter() {
+        for (int i = 0; i < 11; i++) {
+            TagUtils.createTagValue("my-tag-key", "non-printable-character-\u007f");
+        }
+        assertLogsOfCount("Error creating value for tag", 10);
     }
 
 }
