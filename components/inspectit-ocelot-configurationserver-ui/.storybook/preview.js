6ec4de92be7245b8b96155be6c99d736c705cfb6
Marius Oehler <marius.oehler@novatec-gmbh.de>
2021-05-25 09:38:52 +0200

    adding storybook

---
+++ components/inspectit-ocelot-configurationserver-ui/.storybook/preview.js
@@ -0,0 +1,15 @@
+// styles for primereact
+import 'primereact/resources/themes/nova-dark/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+import 'primeflex/primeflex.css';
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    }
+  },
+}
