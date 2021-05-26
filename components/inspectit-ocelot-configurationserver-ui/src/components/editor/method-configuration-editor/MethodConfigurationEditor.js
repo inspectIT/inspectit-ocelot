ff104c3bb3f9c2c45a2a7363f6cef3bd33ab1ee3
Marius Oehler <marius.oehler@novatec-gmbh.de>
2021-05-26 13:29:36 +0200

    adding documentation

--- components/inspectit-ocelot-configurationserver-ui/src/components/editor/method-configuration-editor/MethodConfigurationEditor.js
+++ components/inspectit-ocelot-configurationserver-ui/src/components/editor/method-configuration-editor/MethodConfigurationEditor.js
@@ -6,6 +6,7 @@ import { InputSwitch } from 'primereact/inputswitch';
 import { Button } from 'primereact/button';
 import yaml from 'js-yaml';
 import _ from 'lodash';
+import SelectionInformation from '../SelectionInformation';
 
 /**
  * GUI editor for creating method/configurations.
@@ -16,10 +17,13 @@ const MethodConfigurationEditor = ({ yamlConfiguration }) => {
   const [expandedRows, setExpandedRows] = useState([]);
   const [scopeStates, setScopeStates] = useState({});
 
+  // derrived variables
+  const scopesExist = scopes.length > 0;
+
   useEffect(() => {
     // parse configuration
     const configuration = yaml.safeLoad(yamlConfiguration);
-    
+
     // collect existing scopes from the configuration
     const scopeObjects = _.get(configuration, 'inspectit.instrumentation.scopes');
     const currentScopes = _.map(scopeObjects, (value, key) => {
@@ -44,6 +48,14 @@ const MethodConfigurationEditor = ({ yamlConfiguration }) => {
     setExpandedRows(initialExpandedRows);
   }, []);
 
+  /**
+   * Sets the scope state attribute with the specified name of the scope with the given
+   * name to the provided value.
+   *
+   * @param {*} scopeName the scope's name
+   * @param {*} attribute the attribute to set
+   * @param {*} value the desired value
+   */
   const setScopeStateAttribute = (scopeName, attribute, value) => {
     const scopeState = _.get(scopeStates, scopeName, {});
     setScopeStates({
@@ -55,14 +67,25 @@ const MethodConfigurationEditor = ({ yamlConfiguration }) => {
     });
   };
 
+  /**
+   * Providing the template body for the row group header.
+   */
   const rowGroupHeaderTemplate = ({ typeKey }) => {
-    return <>{typeKey}</>;
+    return 'Target: ' + typeKey;
   };
 
+  /**
+   * Providing the template body for the scopes.
+   */
   const scopeDescriptionBodyTemplate = ({ scope }) => {
-    return <>{JSON.stringify(scope)}</>;
+    return JSON.stringify(scope);
   };
 
+  /**
+   * Providing a generic body template for a scopes state variables (tracing, measuring, ...).
+   * @param {*} scopeName the name of the target scope
+   * @param {*} stateAttribute  the attribute name to set
+   */
   const scopeStateBodyTemplate = (scopeName, stateAttribute) => {
     const scopeState = _.get(scopeStates, scopeName, {});
 
@@ -76,10 +99,13 @@ const MethodConfigurationEditor = ({ yamlConfiguration }) => {
     );
   };
 
+  /**
+   * Providing the template body for the scope's control buttons.
+   */
   const scopeEditBodyTemplate = ({ name }) => {
     return (
       <div align="right">
-        <Button icon="pi pi-pencil" />
+        <Button icon="pi pi-pencil" style={{ marginRight: '0.5rem' }} />
         <Button icon="pi pi-trash" />
       </div>
     );
@@ -88,37 +114,74 @@ const MethodConfigurationEditor = ({ yamlConfiguration }) => {
   return (
     <>
       <style jsx>{`
-        .this :global(.p-datatable) :global(th) {
-          border: 0 none;
+        .this {
+          display: flex;
+          flex-direction: column;
+          height: 100%;
+        }
+
+        .this :global(.p-datatable th) {
+          border: none;
           text-align: left;
+        }
+
+        .this :global(.p-datatable tr td) {
+          border: none;
+        }
+
+        .this :global(.p-datatable tr) {
+          border-top: 1px solid #e7e7e7;
+        }
+
+        .this :global(.p-datatable .p-rowgroup-header td) {
+          padding: 1rem 0.5rem;
+        }
+
+        .this :global(.p-datatable-row td:first-of-type) {
+          padding-left: 2.25rem;
+        }
+
+        /* prevent different backgrounds for even and odd row numbers*/
+        .this :global(.p-datatable .p-datatable-tbody > tr) {
+          background-color: white;
+        }
+
+        .no-content-hint {
+          background-color: red;
+          flex-grow: 1;
         }
       `}</style>
       <div className="this">
-        <DataTable
-          value={scopes}
-          rowHover
-          dataKey="typeKey"
-          sortField="typeKey"
-          sortOrder={1}
-          groupField="typeKey"
-          expandableRowGroups={true}
-          expandedRows={expandedRows}
-          onRowToggle={(e) => setExpandedRows(e.data)}
-          rowGroupHeaderTemplate={rowGroupHeaderTemplate}
-          rowGroupFooterTemplate={() => <></>}
-          rowGroupMode="subheader"
-        >
-          <Column body={scopeDescriptionBodyTemplate} />
-          <Column body={({ name }) => scopeStateBodyTemplate(name, 'tracing')} header="Trace" style={{ width: '6rem' }}></Column>
-          <Column body={({ name }) => scopeStateBodyTemplate(name, 'measure')} header="Measure" style={{ width: '6rem' }}></Column>
-          <Column body={scopeEditBodyTemplate} style={{ width: '8rem' }}></Column>
-        </DataTable>
+        {scopesExist ? (
+          <DataTable
+            value={scopes}
+            rowHover
+            dataKey="typeKey"
+            sortField="typeKey"
+            sortOrder={1}
+            groupField="typeKey"
+            expandableRowGroups={true}
+            expandedRows={expandedRows}
+            onRowToggle={(e) => setExpandedRows(e.data)}
+            rowGroupHeaderTemplate={rowGroupHeaderTemplate}
+            rowGroupFooterTemplate={() => <></>}
+            rowGroupMode="subheader"
+          >
+            <Column body={scopeDescriptionBodyTemplate} header="Target" />
+            <Column body={({ name }) => scopeStateBodyTemplate(name, 'tracing')} header="Trace" style={{ width: '6rem' }}></Column>
+            <Column body={({ name }) => scopeStateBodyTemplate(name, 'measure')} header="Measure" style={{ width: '6rem' }}></Column>
+            <Column body={scopeEditBodyTemplate} style={{ width: '8rem' }}></Column>
+          </DataTable>
+        ) : (
+          <SelectionInformation hint="The configuration is empty." />
+        )}
       </div>
     </>
   );
 };
 
 MethodConfigurationEditor.propTypes = {
+  /** The configuration to show in a YAML representation.*/
   yamlConfiguration: PropTypes.string,
 };
 
--- components/inspectit-ocelot-configurationserver-ui/src/components/editor/method-configuration-editor/MethodConfigurationEditor.stories.js
+++ components/inspectit-ocelot-configurationserver-ui/src/components/editor/method-configuration-editor/MethodConfigurationEditor.stories.js
@@ -13,3 +13,7 @@ export const Default = Template.bind({});
 Default.args = {
     yamlConfiguration: "# {\"type\": \"Method-Configuration\"}\ninspectit:\n  instrumentation:\n    scopes:\n      's_apacheclient_doExecute':\n        interfaces:\n          - name: 'org.apache.http.impl.client.CloseableHttpClient'\n        methods:\n          - name: 'doExecute'\n      \n      's_httpurlconnection_getOutputStream':\n        type:\n          name: 'java.net.HttpURLConnection'\n        methods:\n          - name: 'getOutputStream'\n            arguments: []\n\n      's_httpurlconnection_getInputStream':\n        superclass:\n          name: 'java.net.HttpURLConnection'\n        methods:\n          - name: 'getInputStream'\n            arguments: []\n\n      's_httpurlconnection_connect':\n        superclass:\n          name: 'java.net.HttpURLConnection'\n        methods:\n          - name: 'connect'\n            arguments: []\n\n      's_httpurlconnection_requestInitiators':\n        superclass:\n          name: 'java.net.HttpURLConnection'\n        methods:\n          - name: 'getHeaderField'\n          - name: 'getHeaderFieldDate'\n          - name: 'getHeaderFieldKey'\n          - name: 'getResponseCode'\n          - name: 'getResponseMessage'"
 };
+
+export const Empty = Template.bind({});
+Empty.args = {
+};
