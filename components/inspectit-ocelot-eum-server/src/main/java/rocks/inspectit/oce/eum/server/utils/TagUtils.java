15c40c829f4716f42450cf74ae751425ade5c2db
Mahir Isikli <isikli.mahir@gmail.com>
2021-06-27 11:00:03 +0200

    Implemented Marius' code review changes

--- components/inspectit-ocelot-eum-server/src/main/java/rocks/inspectit/oce/eum/server/utils/TagUtils.java
+++ components/inspectit-ocelot-eum-server/src/main/java/rocks/inspectit/oce/eum/server/utils/TagUtils.java
@@ -8,15 +8,22 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public final class TagUtils {
 
+    //Counter for the number of warnings that have already been printed
     @VisibleForTesting
     static int printedWarningCounter = 0;
 
+    //The time in ms when the last warning was printed
     @VisibleForTesting
     static long lastWarningTime = 0;
 
-    private final static int maxWarningPrints = 10;
+    //The number of maximum warnings that are to be printed
+    private final static int MAX_WARNING_PRINTS = 10;
 
-    private final static int waitingTimeInMilliSeconds = 600000;
+    //The duration in ms that needs to pass, if MAX_WARNING_PRINTS has been reached
+    private final static int WAITING_TIME_IN_MILLI_SECONDS = 600_000;
+
+    //Boolean that indicates whether the user gets a message that further logs are suppressed
+    private static boolean PRINT_FURTHER_MESSAGE = true;
 
     private TagUtils() {
         // empty private default constructor for util class
@@ -44,17 +51,17 @@ public final class TagUtils {
     }
 
     private static void printWarning(String tagKey, String value) {
-        if (printedWarningCounter < maxWarningPrints) {
-            log.warn("Error creating value for tag <{}>: illegal tag value <{}> converted to <invalid>", tagKey, value);
-
-            printedWarningCounter++;
-            if (printedWarningCounter == maxWarningPrints) {
-                lastWarningTime = System.currentTimeMillis();
-            }
-
-        } else if ((System.currentTimeMillis() - lastWarningTime) > waitingTimeInMilliSeconds) {
+        if ((System.currentTimeMillis() - lastWarningTime) > WAITING_TIME_IN_MILLI_SECONDS) {
             printedWarningCounter = 0;
-            printWarning(tagKey, value);
+            PRINT_FURTHER_MESSAGE = true;
+        }
+        if (printedWarningCounter < MAX_WARNING_PRINTS) {
+            log.warn("Error creating value for tag <{}>: illegal tag value <{}> converted to <invalid>", tagKey, value);
+            printedWarningCounter++;
+            lastWarningTime = System.currentTimeMillis();
+        } else if (PRINT_FURTHER_MESSAGE) {
+            log.warn("Further log messages are suppressed");
+            PRINT_FURTHER_MESSAGE = false;
         }
     }
 }
