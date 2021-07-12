7bee73fb7d493583cfc5d99cf3c0071edcd92d0d
Mahir Isikli <isikli.mahir@gmail.com>
2021-07-12 18:17:08 +0200

    First Version without proper Error handling

--- components/inspectit-ocelot-eum-server/src/main/java/rocks/inspectit/oce/eum/server/rest/TraceController.java
+++ components/inspectit-ocelot-eum-server/src/main/java/rocks/inspectit/oce/eum/server/rest/TraceController.java
@@ -1,5 +1,7 @@
 package rocks.inspectit.oce.eum.server.rest;
 
+import com.google.common.base.Stopwatch;
+import com.google.common.collect.ImmutableMap;
 import com.google.protobuf.InvalidProtocolBufferException;
 import com.google.protobuf.util.JsonFormat;
 import io.opentelemetry.proto.collector.trace.v1.ExportTraceServiceRequest;
@@ -15,12 +17,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
+import rocks.inspectit.oce.eum.server.metrics.SelfMonitoringMetricManager;
 import rocks.inspectit.oce.eum.server.tracing.opentelemtry.OpenTelemetryProtoConverter;
 
 import javax.validation.constraints.NotBlank;
-import java.nio.charset.StandardCharsets;
 import java.util.Collection;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @RestController()
 @Slf4j
@@ -32,9 +35,14 @@ public class TraceController {
     @Autowired(required = false)
     private List<SpanExporter> spanExporters;
 
+    @Autowired
+    private SelfMonitoringMetricManager selfMonitoring;
+
     @CrossOrigin
     @PostMapping("spans")
     public ResponseEntity<Void> spans(@RequestBody @NotBlank String data) {
+        Stopwatch stopwatch = Stopwatch.createUnstarted();
+
         // if we have no exporter return ok, ignore data
         if (CollectionUtils.isEmpty(spanExporters)) {
             return ResponseEntity.ok().build();
@@ -46,11 +54,19 @@ public class TraceController {
             JsonFormat.parser().merge(data, requestBuilder);
             ExportTraceServiceRequest request = requestBuilder.build();
 
+            stopwatch.start();
+
             // then convert using out converter
             Collection<SpanData> spans = converter.convert(request);
 
             // export data in each exporter
             spanExporters.forEach(exporter -> exporter.export(spans));
+
+            stopwatch.stop();
+            ImmutableMap<String, String> tagMap = ImmutableMap.of("trace_controller", "trace_controller", "is_error", String
+                    .valueOf(false));
+            selfMonitoring.record("trace_controller_duration", stopwatch.elapsed(TimeUnit.MILLISECONDS), tagMap);
+            selfMonitoring.record("trace_controller_span_size", spans.size(), tagMap);
 
             // return accepted
             return ResponseEntity.accepted().build();
