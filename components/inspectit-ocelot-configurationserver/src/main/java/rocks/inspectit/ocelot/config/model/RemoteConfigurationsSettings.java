fb546953e1795c319272199ed6162dde3f0e3e4a
Marius Oehler <marius.oehler@novatec-gmbh.de>
2021-07-14 11:20:27 +0200

    Add comments on settings

--- components/inspectit-ocelot-configurationserver/src/main/java/rocks/inspectit/ocelot/config/model/RemoteConfigurationsSettings.java
+++ components/inspectit-ocelot-configurationserver/src/main/java/rocks/inspectit/ocelot/config/model/RemoteConfigurationsSettings.java
@@ -6,43 +6,74 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.apache.commons.lang3.StringUtils;
 import org.eclipse.jgit.transport.URIish;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 
 import javax.validation.constraints.AssertFalse;
 import javax.validation.constraints.AssertTrue;
 import javax.validation.constraints.NotNull;
-import java.net.URI;
 
+/**
+ * Settings for connecting the configuration server to remote Git repositories.
+ */
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class RemoteConfigurationsSettings {
 
+    /**
+     * Supported authentication types.
+     */
+    public enum AuthenticationType {
+        NONE, PASSWORD, PPK;
+    }
+
+    /**
+     * Whether remote Git repositories should be used for configuration management.
+     */
     @Builder.Default
     private boolean enabled = false;
 
+    /**
+     * The name of the remote ref in the local Git repository.
+     */
     private String remoteName;
 
+    /**
+     * The URI to the remote Git repository.
+     */
     private URIish gitRepositoryUri;
 
+    /**
+     * The branch name on the remote Git repository which will be used to fetch workspace-configurations from.
+     */
     private String sourceBranch;
 
+    /**
+     * The branch name on the remote Git repository which will be used to push live-configurations to.
+     */
     private String targetBranch;
 
+    /**
+     * The type of authentication to use.
+     */
     @Builder.Default
     @NotNull
     private AuthenticationType authenticationType = AuthenticationType.NONE;
 
+    /**
+     * The username for accessing the remote repository. Only used in case of PASSWORD authentication.
+     */
     private String username;
 
+    /**
+     * The password for accessing the remote repository. Only used in case of PASSWORD authentication.
+     */
     private String password;
 
+    /**
+     * The private key to use for SSH authentication. Only used in case of PPK authentication.
+     */
     private String privateKeyFile;
-
-    public enum AuthenticationType {
-        NONE, PASSWORD, PPK;
-    }
 
     @AssertFalse
     public boolean isRemoteNameBlank() {
