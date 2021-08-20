9fbb3710e32b3cba192420febeb12d1feb96ff2a
timwildberger <80902438+timwildberger@users.noreply.github.com>
2021-08-20 11:26:25 +0200

    Closes #1093 - The demo app doesn't work (#1197)

--- inspectit-ocelot-demo/load/docker-entrypoint.sh
+++ inspectit-ocelot-demo/load/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/ash
 set -e
 
-until $(curl --output /dev/null --silent --head --fail http://api-gateway:8080); do
+until $(curl --output /dev/null --silent --fail http://api-gateway:8080); do
   printf 'waiting for API Gateway ...\n'
   sleep 10
 done
