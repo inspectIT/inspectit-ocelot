d25a6a0e2c837f30a35d494c50cf3dc4add8de22
Heiko Holz <heiko.holz@novatec-gmbh.de>
2022-01-11 11:13:55 +0100

    Removed OpenCensusAgentMetricsExporterService and OpenCensusAgentTraceExporterService that are no longer supported with OpenTelemetry (#1246)

--- inspectit-ocelot-core/src/test/java/rocks/inspectit/ocelot/core/exporter/OpenTelemetryAgentTraceExporterServiceIntTest.java
+++
@@ -1,6 +0,0 @@
-package rocks.inspectit.ocelot.core.exporter;
-
-// TODO: implement this test class according to the previously used OpenCensusAgentTraceExporterServiceIntTest
-public class OpenTelemetryAgentTraceExporterServiceIntTest {
-
-}
