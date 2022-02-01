857cd9edcb1b036d28e7728b17d97547dacbc3d6
aaronweissler <46028218+aaronweissler@users.noreply.github.com>
2022-02-01 12:33:54 +0100

    Testing

--- components/inspectit-ocelot-config-doc-generator/src/main/java/inspectit/ocelot/config/doc/generator/docobjects/RuleDoc.java
+++ components/inspectit-ocelot-config-doc-generator/src/main/java/inspectit/ocelot/config/doc/generator/docobjects/RuleDoc.java
@@ -1,10 +1,12 @@
 package inspectit.ocelot.config.doc.generator.docobjects;
 
 import lombok.Getter;
+import lombok.Setter;
 
 import java.util.*;
 
 @Getter
+@Setter
 public class RuleDoc extends BaseDoc {
 
     public RuleDoc(String name, String description, List<String> include, List<String> scopes,
@@ -18,35 +20,41 @@ public class RuleDoc extends BaseDoc {
         this.entryExits = entryExits;
     }
 
-    List<String> include;
-    List<String> scopes;
-    List<RuleMetricsDoc> metricsDocs;
+    public RuleDoc(String name){
+        super(name, "");
+    }
+
+    List<String> include = Collections.emptyList();
+    List<String> scopes = Collections.emptyList();
+    List<RuleMetricsDoc> metricsDocs = Collections.emptyList();
     RuleTracingDoc tracingDoc;
-    Map<String, Map<String, RuleActionCallDoc>> entryExits;
+    Map<String, Map<String, RuleActionCallDoc>> entryExits = Collections.emptyMap();
 
     public void addEntryExitFromIncludedRules(Map<String, RuleDoc> allRuleDocs, List<String> includedRules){
 
         for(String includedRuleName: includedRules){
 
             RuleDoc includedRule = allRuleDocs.get(includedRuleName);
-            Map<String, Map<String, RuleActionCallDoc>> includedRuleEntryExits = includedRule.getEntryExits();
+            if(includedRule != null){
+                Map<String, Map<String, RuleActionCallDoc>> includedRuleEntryExits = includedRule.getEntryExits();
 
-            for(String includedRuleEntryExitKey: includedRuleEntryExits.keySet()){
+                for(String includedRuleEntryExitKey: includedRuleEntryExits.keySet()){
 
-                if(!entryExits.containsKey(includedRuleEntryExitKey)){
-                    entryExits.put(includedRuleEntryExitKey, new TreeMap<>());
-                }
-                for(RuleActionCallDoc entryExitActionCall: includedRuleEntryExits.get(includedRuleEntryExitKey).values()){
+                    if(!entryExits.containsKey(includedRuleEntryExitKey)){
+                        entryExits.put(includedRuleEntryExitKey, new TreeMap<>());
+                    }
+                    for(RuleActionCallDoc entryExitActionCall: includedRuleEntryExits.get(includedRuleEntryExitKey).values()){
 
-                    Map<String, RuleActionCallDoc> actionCallDocs = entryExits.get(includedRuleEntryExitKey);
+                        Map<String, RuleActionCallDoc> actionCallDocs = entryExits.get(includedRuleEntryExitKey);
 
-                    if(!actionCallDocs.containsKey(entryExitActionCall.getName())) {
-                        actionCallDocs.put(entryExitActionCall.getName(), new RuleActionCallDoc(entryExitActionCall,
-                                includedRule.getName()));
+                        if(!actionCallDocs.containsKey(entryExitActionCall.getName())) {
+                            actionCallDocs.put(entryExitActionCall.getName(), new RuleActionCallDoc(entryExitActionCall,
+                                    includedRule.getName()));
+                        }
                     }
                 }
+                addEntryExitFromIncludedRules(allRuleDocs, includedRule.getInclude());
             }
-            addEntryExitFromIncludedRules(allRuleDocs, includedRule.getInclude());
         }
     }
 }
