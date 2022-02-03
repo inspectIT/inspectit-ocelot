2759f5548b32e07b6713973c36166200b5e702a7
aaronweissler <46028218+aaronweissler@users.noreply.github.com>
2022-02-03 11:59:41 +0100

    Refactor RuleDoc.addEntryExitFromIncludedRules to use Streams

--- components/inspectit-ocelot-config-doc-generator/src/main/java/inspectit/ocelot/config/doc/generator/docobjects/RuleDoc.java
+++ components/inspectit-ocelot-config-doc-generator/src/main/java/inspectit/ocelot/config/doc/generator/docobjects/RuleDoc.java
@@ -37,29 +37,35 @@ public class RuleDoc extends BaseDoc {
 
     public void addEntryExitFromIncludedRules(Map<String, RuleDoc> allRuleDocs, List<String> includedRules){
 
-        for(String includedRuleName: includedRules){
+        // Iterate through all included Rules' RuleDocs
+        includedRules.stream().map(allRuleDocs::get).filter(Objects::nonNull).forEach(includedRule -> {
 
-            RuleDoc includedRule = allRuleDocs.get(includedRuleName);
-            if(includedRule != null){
-                Map<String, Map<String, RuleActionCallDoc>> includedRuleEntryExits = includedRule.getEntryExits();
-
-                for(String includedRuleEntryExitKey: includedRuleEntryExits.keySet()){
-
-                    if(!entryExits.containsKey(includedRuleEntryExitKey)){
-                        entryExits.put(includedRuleEntryExitKey, new TreeMap<>());
-                    }
-                    for(RuleActionCallDoc entryExitActionCall: includedRuleEntryExits.get(includedRuleEntryExitKey).values()){
-
-                        Map<String, RuleActionCallDoc> actionCallDocs = entryExits.get(includedRuleEntryExitKey);
-
-                        if(!actionCallDocs.containsKey(entryExitActionCall.getName())) {
-                            actionCallDocs.put(entryExitActionCall.getName(), new RuleActionCallDoc(entryExitActionCall,
-                                    includedRule.getName()));
-                        }
-                    }
+            // In each RuleDoc iterate through its entries in entryExits,
+            // i. e. entry, exit, pre-entry, pre-exit, etc.
+            includedRule.getEntryExits().forEach((includedEEKey, includedEnExValue) -> {
+                // If the original Rule does not have a key in entryExits that an included Rule has, an empty TreeMap
+                // is added at that point for future usage.
+                if (!entryExits.containsKey(includedEEKey)) {
+                    entryExits.put(includedEEKey, new TreeMap<>());
                 }
-                addEntryExitFromIncludedRules(allRuleDocs, includedRule.getInclude());
-            }
-        }
+
+                // Iterate through the entries in one entryExit entry, i. e. the attributes defined for this state.
+                includedEnExValue.entrySet()
+                        .stream()
+                        // If an attribute already is in the entryExits Map of the original Rule, that means the
+                        // original Rule has overwritten the attribute and the attribute from the included Rule
+                        // does not need to be added.
+                        .filter(actionCallEntry -> !entryExits.get(includedEEKey)
+                                .containsKey(actionCallEntry.getKey()))
+                        // Add the remaining attributes from the included Rule to the original Rule with a reference
+                        // to the included Rule to know where it came from.
+                        .forEach(actionCallEntry -> {
+                            entryExits.get(includedEEKey)
+                                    .put(actionCallEntry.getKey(), new RuleActionCallDoc(
+                                            actionCallEntry.getValue(), includedRule.getName()));
+                        });
+            });
+            addEntryExitFromIncludedRules(allRuleDocs, includedRule.getInclude());
+        });
     }
 }
