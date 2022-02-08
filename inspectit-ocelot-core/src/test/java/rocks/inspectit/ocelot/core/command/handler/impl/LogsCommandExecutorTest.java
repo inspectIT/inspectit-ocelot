5e7330273a54d6d1c5a9cb0236a331df47ebb576
Henning Schulz <henning.schulz@novatec-gmbh.de>
2022-02-08 15:56:22 +0100

    add test case describing expected behavior of LogsCommandExecutor

--- inspectit-ocelot-core/src/test/java/rocks/inspectit/ocelot/core/command/handler/impl/LogsCommandExecutorTest.java
+++ inspectit-ocelot-core/src/test/java/rocks/inspectit/ocelot/core/command/handler/impl/LogsCommandExecutorTest.java
@@ -1,23 +1,36 @@
 package rocks.inspectit.ocelot.core.command.handler.impl;
 
 import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.spi.LoggingEvent;
 import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.TestPropertySource;
+import rocks.inspectit.ocelot.commons.models.command.impl.LogsCommand;
 import rocks.inspectit.ocelot.config.model.selfmonitoring.LogPreloadingSettings;
 import rocks.inspectit.ocelot.core.SpringTestBase;
 import rocks.inspectit.ocelot.core.config.InspectitEnvironment;
+import rocks.inspectit.ocelot.core.selfmonitoring.LogPreloader;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
+import java.util.Arrays;
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.awaitility.Awaitility.await;
 
+@ExtendWith(MockitoExtension.class)
 public class LogsCommandExecutorTest {
 
     static final String tmpDir = "tmpconf_int_test";
@@ -67,6 +80,47 @@ public class LogsCommandExecutorTest {
                     assertThat(settings.getBufferSize()).isEqualTo(bufferSize);
                 }
             });
+        }
+
+    }
+
+    @Nested
+    @DirtiesContext
+    public class Execute {
+
+        @Mock
+        LogPreloader preloader;
+
+        @InjectMocks
+        LogsCommandExecutor executor;
+
+        @Test
+        public void testLogFormat() {
+            Mockito.when(preloader.getPreloadedLogs())
+                    .thenReturn(Arrays.asList(new LoggingEvent("com.dummy.Method", (Logger) LoggerFactory.getLogger(LogsCommandExecutorTest.class), Level.ERROR, "Message", new Throwable(), new String[]{}), new LoggingEvent("com.dummy.Method", (Logger) LoggerFactory.getLogger(LogsCommandExecutorTest.class), Level.WARN, "Message {}", new Throwable(), new String[]{"Foo"}), new LoggingEvent("com.dummy.Method", (Logger) LoggerFactory.getLogger(LogsCommandExecutorTest.class), Level.ERROR, "Exception", new RuntimeException(), new String[]{})));
+
+            LogsCommand command = new LogsCommand();
+            command.setCommandId(UUID.randomUUID());
+            LogsCommand.Response response = (LogsCommand.Response) executor.execute(command);
+
+            assertThat(response.getResult().split("\\n")).hasSize(3);
+
+            // should contain log levels
+            assertThat(response.getResult()).contains("WARN");
+            assertThat(response.getResult()).contains("ERROR");
+
+            // should contain messages
+            assertThat(response.getResult()).contains("Message");
+            assertThat(response.getResult()).contains("Message Foo");
+            assertThat(response.getResult()).contains("Exception");
+
+            // should contain the exception
+            assertThat(response.getResult()).contains("RuntimeException");
+
+            // should contain class that logged the message
+            assertThat(response.getResult()).contains("LogsCommandExecutorTest");
+
+            // TODO: check if timestamps are present in the appropriate format
         }
 
     }
