d4f5ef1f046bddb64cd364ecf38c617a83a5d28b
aaronweissler <46028218+aaronweissler@users.noreply.github.com>
2022-02-10 09:48:23 +0100

    Fix to ActionDocSettings to deal with build warning

--- inspectit-ocelot-config/src/main/java/rocks/inspectit/ocelot/config/model/instrumentation/documentation/ActionDocSettings.java
+++ inspectit-ocelot-config/src/main/java/rocks/inspectit/ocelot/config/model/instrumentation/documentation/ActionDocSettings.java
@@ -1,12 +1,14 @@
 package rocks.inspectit.ocelot.config.model.instrumentation.documentation;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import javax.validation.constraints.NotBlank;
 import java.util.HashMap;
 import java.util.Map;
 
 @Data
+@EqualsAndHashCode(callSuper=true)
 public class ActionDocSettings extends BaseDocSettings {
 
     private Map<@NotBlank String, @NotBlank String> inputDesc = new HashMap<>();
