54cb5e3d76486e09161b0fe9b0ffb70b0be95e08
aaronweissler <46028218+aaronweissler@users.noreply.github.com>
2022-03-01 16:39:20 +0100

    Small fix to JavaDoc

--- inspectit-ocelot-config/src/main/java/rocks/inspectit/ocelot/config/model/exporters/ExporterEnabledState.java
+++ inspectit-ocelot-config/src/main/java/rocks/inspectit/ocelot/config/model/exporters/ExporterEnabledState.java
@@ -2,7 +2,7 @@ package rocks.inspectit.ocelot.config.model.exporters;
 
 /**
  * Enum to change behaviour of exporters.
- * If set to ENABLED, the exporter will try to start. If any needed settings are not set, the starting will be aborted and an error logged.
+ * If set to ENABLED, the exporter will try to start. If any needed settings are not set, the starting will be aborted and a warning logged.
  * If set to IF_CONFIGURED, the exporter will try to start if all needed settings are set, otherwise it will silently stay disabled.
  * If set to DISABLED, the exporter will not start.
  */
