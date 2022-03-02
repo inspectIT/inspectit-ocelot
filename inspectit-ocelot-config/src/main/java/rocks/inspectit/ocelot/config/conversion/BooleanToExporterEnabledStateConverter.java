7168bba7fce07be5fb363055736348eb9e9e02e5
aaronweissler <46028218+aaronweissler@users.noreply.github.com>
2022-03-02 13:05:08 +0100

    Add Documentation to Converter

--- inspectit-ocelot-config/src/main/java/rocks/inspectit/ocelot/config/conversion/BooleanToExporterEnabledStateConverter.java
+++ inspectit-ocelot-config/src/main/java/rocks/inspectit/ocelot/config/conversion/BooleanToExporterEnabledStateConverter.java
@@ -4,6 +4,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.convert.converter.Converter;
 import rocks.inspectit.ocelot.config.model.exporters.ExporterEnabledState;
 
+/**
+ * Before version 1.15, the enabled property of exporters was a Boolean, now it is a {@link ExporterEnabledState}.
+ * To support not-yet-updated configurations, this converter converts the old Boolean values to their equivalent new values,
+ * i.e. 'true' to 'IF_CONFIGURED' and 'false' to 'DISABLED'.
+ * The class is deprecated, since this conversion functionality will be removed and configurations using the old style
+ * made invalid in a future release of InspectIT Ocelot.
+ */
 @Slf4j
 @Deprecated
 public class BooleanToExporterEnabledStateConverter implements Converter<Boolean, ExporterEnabledState> {
