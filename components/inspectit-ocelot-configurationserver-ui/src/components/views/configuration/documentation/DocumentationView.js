c71e4a3ab60898988af5a7d5e8774b83e240ace2
Marius Oehler <mariusoe@users.noreply.github.com>
2022-03-04 13:49:22 +0100

    feat(ui): ability to filter configuration docs (#1341)

--- components/inspectit-ocelot-configurationserver-ui/src/components/views/configuration/documentation/DocumentationView.js
+++ components/inspectit-ocelot-configurationserver-ui/src/components/views/configuration/documentation/DocumentationView.js
@@ -2,8 +2,12 @@ import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { Dropdown } from 'primereact/dropdown';
 import { Checkbox } from 'primereact/checkbox';
+import { InputText } from 'primereact/inputtext';
+import { Button } from 'primereact/button';
 import ConfigDocumentation from './ConfigDocumentation';
 import useFetchData from '../../../../hooks/use-fetch-data';
+import _ from 'lodash';
+import useDebounce from '../../../../hooks/use-debounce';
 
 /**
  * The sidebar panel for showing existing versions of the configuration files.
@@ -15,10 +19,15 @@ const DocumentationView = () => {
   // local state
   const [selectedAgentMapping, setSelectedAgentMapping] = useState(null);
   const [includeDefault, setIncludeDefault] = useState(true);
+  const [filterValue, setFilterValue] = useState('');
+  const [configurationDocs, setConfigurationDocs] = useState(null);
+
+  // debounced filter value for preventing filtering on each input change
+  const filterValueDebounced = useDebounce(filterValue, 500);
 
   // fetch required data
   const [{ data: agentMappings }, refreshAgentMappings] = useFetchData('mappings', {}, []);
-  const [{ data: configurationDocs, isLoading: isLoadingDocs, isError: isDocsError, error }, refreshConfigurationDocs] = useFetchData(
+  const [{ data: fullConfigurationDocs, isLoading: isLoadingDocs, isError: isDocsError, error }, refreshConfigurationDocs] = useFetchData(
     'configuration/documentation',
     {
       'agent-mapping': selectedAgentMapping,
@@ -40,6 +49,20 @@ const DocumentationView = () => {
       refreshConfigurationDocs();
     }
   }, [selectedAgentMapping, includeDefault, configurationUpdateDate]);
+
+  // filter documentation for display
+  useEffect(() => {
+    if (fullConfigurationDocs) {
+      const filter = filterValueDebounced.toLowerCase();
+      const filteredData = {
+        actions: _.filter(fullConfigurationDocs.actions, (e) => e.name.toLowerCase().includes(filter)),
+        scopes: _.filter(fullConfigurationDocs.scopes, (e) => e.name.toLowerCase().includes(filter)),
+        rules: _.filter(fullConfigurationDocs.rules, (e) => e.name.toLowerCase().includes(filter)),
+        metrics: _.filter(fullConfigurationDocs.metrics, (e) => e.name.toLowerCase().includes(filter)),
+      };
+      setConfigurationDocs(filteredData);
+    }
+  }, [filterValueDebounced, fullConfigurationDocs]);
 
   return (
     <>
@@ -89,6 +112,10 @@ const DocumentationView = () => {
             padding: 0.5rem;
             font-family: monospace;
           }
+          .filter {
+            padding: 0.5rem 1rem;
+            border-bottom: 1px solid #9e9e9e;
+          }
         `}
       </style>
       <div className="documentation p-component">
@@ -110,6 +137,20 @@ const DocumentationView = () => {
             <label htmlFor="defaultConfigurationCb">Include Default Configuration:</label>
             <Checkbox id="defaultConfigurationCb" checked={includeDefault} onChange={(e) => setIncludeDefault(e.checked)} />
           </div>
+        </div>
+
+        <div className="filter p-inputgroup">
+          <span className="pi p-inputgroup-addon pi-filter" />
+
+          <InputText
+            style={{ flex: 1 }}
+            value={filterValue}
+            placeholder={'Filter Documentation'}
+            onChange={(e) => setFilterValue(e.target.value)}
+            disabled={!(configurationDocs && !isDocsError)}
+          />
+
+          <Button icon="pi pi-times" disabled={!filterValue} onClick={() => setFilterValue('')} />
         </div>
 
         {selectedAgentMapping ? (
