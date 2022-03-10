ea323a5477e15b28904a2b42b946e59d96d38784
aaronweissler <46028218+aaronweissler@users.noreply.github.com>
2022-03-10 16:24:51 +0100

    Enhance reconnecting with backoff

--- inspectit-ocelot-core/src/main/java/rocks/inspectit/ocelot/core/command/AgentCommandService.java
+++ inspectit-ocelot-core/src/main/java/rocks/inspectit/ocelot/core/command/AgentCommandService.java
@@ -4,6 +4,7 @@ import com.google.common.annotations.VisibleForTesting;
 import io.grpc.Channel;
 import io.grpc.ManagedChannelBuilder;
 import io.grpc.stub.StreamObserver;
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,21 +20,32 @@ import rocks.inspectit.ocelot.grpc.FirstResponse;
 import java.lang.management.ManagementFactory;
 import java.lang.management.RuntimeMXBean;
 import java.net.URL;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Service responsible for fetching agent commands.
  */
 @Service
 @Slf4j
+@Getter
 public class AgentCommandService extends DynamicallyActivatableService {
 
     /**
-     * Used to delegate recieved {@link Command} objects to their respective implementation of {@link rocks.inspectit.ocelot.core.command.handler.CommandExecutor}.
+     * Used to delegate received {@link Command} objects to their respective implementation of {@link rocks.inspectit.ocelot.core.command.handler.CommandExecutor}.
      */
     @Autowired
     private CommandDelegator commandDelegator;
 
-    private StreamObserver<CommandResponse> commandResponseObserver = null;
+    // I think volatile is needed because of the possibility of retrying connecting and disabling at the same time
+    volatile private StreamObserver<CommandResponse> commandResponseObserver = null;
+
+    private long lastRetryAttempt;
+
+    ScheduledThreadPoolExecutor reconnectExecutor = new ScheduledThreadPoolExecutor(1);
+
+    private int retriesAttempted = 0;
 
     public AgentCommandService() {
         super("agentCommands");
@@ -73,17 +85,18 @@ public class AgentCommandService extends DynamicallyActivatableService {
 
             log.info("Connecting to Configserver over grpc for agent commands over URL '{}' with agent ID '{}'", commandsUrl, agentId);
 
-            return startAskForCommandsConnection(asyncStub, agentId);
-            
+            return startAskForCommandsConnection(configuration.getAgentCommands(), asyncStub, agentId);
+
         } catch (Exception e) {
             log.error("Could not enable the agent command service.", e);
             return false;
         }
     }
 
-    private boolean startAskForCommandsConnection(AgentCommandsGrpc.AgentCommandsStub asyncStub, String agentId) {
+    private boolean startAskForCommandsConnection(AgentCommandSettings settings, AgentCommandsGrpc.AgentCommandsStub asyncStub, String agentId) {
         try {
-            commandResponseObserver = asyncStub.askForCommands(new StreamObserver<Command>() {
+
+            StreamObserver<Command> commandObserver = new StreamObserver<Command>() {
                 @Override
                 public void onNext(Command command) {
                     try {
@@ -99,15 +112,30 @@ public class AgentCommandService extends DynamicallyActivatableService {
                 @Override
                 public void onError(Throwable t) {
                     log.error("Encountered error in exchangeInformation ending the stream connection with config-Server. {}", t.toString());
-                    commandResponseObserver = null;
 
-                    boolean success = startAskForCommandsConnection(asyncStub, agentId);
-                    if (success) {
-                        log.info("Successfully restarted connection after error.");
-                    } else {
-                        disable();
-                        log.info("Could not restart connection after error.");
+                    long currentTime = System.nanoTime();
+
+                    if (retriesAttempted > 0) {
+                        if ((currentTime - lastRetryAttempt) > (Math.pow(10, 9) * settings.getBackoffResetTime())) {
+                            retriesAttempted = 0;
+                        }
                     }
+
+                    ScheduledFuture<?> restartFuture = reconnectExecutor.schedule(() -> {
+                        boolean success = startAskForCommandsConnection(settings, asyncStub, agentId);
+                        if (success) {
+                            log.info("Successfully restarted connection after error.");
+                        } else {
+                            disable();
+                            log.info("Could not restart connection after error.");
+                        }
+                    }, (long) Math.pow(2, retriesAttempted), TimeUnit.SECONDS);
+
+                    if (retriesAttempted < settings.getMaxBackoffIncreases()) {
+                        retriesAttempted++;
+                    }
+
+                    lastRetryAttempt = currentTime;
                 }
 
                 @Override
@@ -116,13 +144,16 @@ public class AgentCommandService extends DynamicallyActivatableService {
                     commandResponseObserver = null;
                 }
 
-            });
+            };
+
+            commandResponseObserver = asyncStub.askForCommands(commandObserver);
 
             commandResponseObserver.onNext(CommandResponse.newBuilder()
                     .setFirst(FirstResponse.newBuilder().setAgentId(agentId))
                     .buildPartial());
         } catch (Exception e) {
             log.error("Could not start askForCommands-connection.", e);
+            commandResponseObserver = null;
             return false;
         }
 
@@ -133,8 +164,14 @@ public class AgentCommandService extends DynamicallyActivatableService {
     protected boolean doDisable() {
         log.info("Stopping agent command service.");
 
+        reconnectExecutor.shutdownNow();
+
         if (commandResponseObserver != null) {
-            commandResponseObserver.onCompleted();
+            try {
+                commandResponseObserver.onCompleted();
+            } catch (NullPointerException e) {
+                log.debug("Encountered null exception while disabling agent command service, probably because a thread trying to restart the connection was running and modifying commandResponseObserver at the same time.", e);
+            }
             commandResponseObserver = null;
         }
         return true;
@@ -146,10 +183,12 @@ public class AgentCommandService extends DynamicallyActivatableService {
 
         if (settings.isDeriveFromHttpConfigUrl()) {
             URL url = configuration.getConfig().getHttp().getUrl();
+            Integer port = settings.getAgentCommandPort();
             if (url == null) {
-                throw new IllegalStateException("The URL cannot derived from the HTTP configuration URL because it is null.");
+                throw new IllegalStateException("The URL cannot be derived from the HTTP configuration URL because it is null.");
+            } else if (port == null) {
+                throw new IllegalStateException("The URL cannot be derived from the HTTP configuration URL because the agentCommandPort is null.");
             }
-
             return String.format("%s:%s", url.getHost(), settings.getAgentCommandPort());
         } else {
             return settings.getUrl();
