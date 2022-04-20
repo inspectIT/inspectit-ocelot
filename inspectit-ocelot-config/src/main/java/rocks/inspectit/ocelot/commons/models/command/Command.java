921e959b2b483aeb617cd2aa8300688a76f3eb58
Mahir Isikli <isikli.mahir@gmail.com>
2022-04-20 16:41:34 +0200

    Added another json sub type for ListDependencies

--- inspectit-ocelot-config/src/main/java/rocks/inspectit/ocelot/commons/models/command/Command.java
+++ inspectit-ocelot-config/src/main/java/rocks/inspectit/ocelot/commons/models/command/Command.java
@@ -1,13 +1,13 @@
 package rocks.inspectit.ocelot.commons.models.command;
 
-
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import rocks.inspectit.ocelot.commons.models.command.impl.ListClassesCommand;
-import rocks.inspectit.ocelot.commons.models.command.impl.PingCommand;
+import rocks.inspectit.ocelot.commons.models.command.impl.ListDependenciesCommand;
 import rocks.inspectit.ocelot.commons.models.command.impl.LogsCommand;
+import rocks.inspectit.ocelot.commons.models.command.impl.PingCommand;
 
 import java.util.UUID;
 
@@ -17,11 +17,7 @@ import java.util.UUID;
 @Data
 @AllArgsConstructor
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "type")
-@JsonSubTypes(value = {
-        @JsonSubTypes.Type(name = PingCommand.TYPE_IDENTIFIER, value = PingCommand.class),
-        @JsonSubTypes.Type(name = ListClassesCommand.TYPE_IDENTIFIER, value = ListClassesCommand.class),
-        @JsonSubTypes.Type(name = LogsCommand.TYPE_IDENTIFIER, value = LogsCommand.class)
-})
+@JsonSubTypes(value = {@JsonSubTypes.Type(name = PingCommand.TYPE_IDENTIFIER, value = PingCommand.class), @JsonSubTypes.Type(name = ListClassesCommand.TYPE_IDENTIFIER, value = ListClassesCommand.class), @JsonSubTypes.Type(name = LogsCommand.TYPE_IDENTIFIER, value = LogsCommand.class), @JsonSubTypes.Type(name = ListDependenciesCommand.TYPE_IDENTIFIER, value = ListDependenciesCommand.class)})
 public abstract class Command {
 
     /**
@@ -33,7 +29,7 @@ public abstract class Command {
      * Basic constructor which creates a UUID for this command.
      */
     public Command() {
-        this.commandId = UUID.randomUUID();
+        commandId = UUID.randomUUID();
     }
 
 }
