c1ac32221dc15cd0dfeb4f9f08e8bb945a0a1d86
Simon Braitsch <simon.braitsch@novatec-gmbh.de>
2022-05-31 14:02:08 +0200

    feat 1454: Display Agent logs in Config Server UI

---
+++ components/inspectit-ocelot-configurationserver-ui/src/components/views/mappings/ContentTypeMapper.js
@@ -0,0 +1,32 @@
+/**
+ * mapping function that returns all values needed to setup the download of a file for a given content-type
+ * @param contentType
+ * @param contextName
+ * @returns {{fileExtension: string, header: string, language: string, mimeType: string}}
+ * @constructor
+ */
+export const ContentTypeMapper = (contentType, contextName) => {
+  switch (contentType) {
+    case 'config':
+      return {
+        language: 'yaml',
+        fileExtension: '.yml',
+        mimeType: 'text/x-yaml',
+        header: 'Configuration of ' + contextName,
+      };
+    case 'log':
+      return {
+        language: 'plaintext',
+        fileExtension: '.log',
+        mimeType: 'text/plain',
+        header: 'Logs of ' + contextName,
+      };
+    default:
+      return {
+        language: 'plaintext',
+        fileExtension: '.txt',
+        mimeType: 'text/plain',
+        header: 'Content of ' + contextName,
+      };
+  }
+};
