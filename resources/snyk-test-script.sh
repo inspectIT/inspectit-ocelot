657117e670120ad6fd061c43340b93cddd927c6a
aaronweissler <46028218+aaronweissler@users.noreply.github.com>
2022-06-20 08:46:11 +0200

    Add shell script for snyk

---
+++ resources/snyk-test-script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+snyk test --file=build.gradle --all-sub-projects --sarif-file-output=snyk_test_result.sarif
+exit_code=$?
+
+if (( exit_code == 0)); then
+  echo "No issues found by Snyk. Will upload SARIF."
+  (exit 0)
+elif (( exit_code == 1)); then
+  echo "Issues found by Snyk. Will upload SARIF."
+  (exit 0)
+else
+  echo "Snyk command failed, will not upload SARIF."
+  (exit 1)
+fi
