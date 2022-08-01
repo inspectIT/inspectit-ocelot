adaee84bec8723576e4b1294a66c57721aa166ee
Heiko Holz <heiko.holz@novatec-gmbh.de>
2022-08-01 11:00:14 +0200

    change default state of showHiddenFiles

--- components/inspectit-ocelot-configurationserver-ui/src/redux/ducks/initial-states.js
+++ components/inspectit-ocelot-configurationserver-ui/src/redux/ducks/initial-states.js
@@ -25,7 +25,7 @@ const configuration = {
   /** The existing configuration files. */
   files: [],
   /** Indicates if hidden files ares shown. */
-  showHiddenFiles: true,
+  showHiddenFiles: false,
   /**
    * The history of file-move operations which were executed successfully.
    * When the files are refetched, it can occur that the selection points to a non existing file.
