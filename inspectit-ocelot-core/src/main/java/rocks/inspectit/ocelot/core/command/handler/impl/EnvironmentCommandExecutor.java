d52469bd97d95102b50cfa655852db57ef9531b9
Jochen Just <jochen.just@novatec-gmbh.de>
2022-11-08 15:55:02 +0100

    Implements EnvironmentCommand via gRPC

--- inspectit-ocelot-core/src/main/java/rocks/inspectit/ocelot/core/command/handler/impl/EnvironmentCommandExecutor.java
+++ inspectit-ocelot-core/src/main/java/rocks/inspectit/ocelot/core/command/handler/impl/EnvironmentCommandExecutor.java
@@ -3,15 +3,48 @@ package rocks.inspectit.ocelot.core.command.handler.impl;
 import org.springframework.stereotype.Component;
 import rocks.inspectit.ocelot.commons.models.command.impl.EnvironmentCommand;
 import rocks.inspectit.ocelot.core.command.handler.CommandExecutor;
+import rocks.inspectit.ocelot.grpc.Command;
+import rocks.inspectit.ocelot.grpc.CommandResponse;
+import rocks.inspectit.ocelot.grpc.EnvironmentCommandResponse;
 
 import java.lang.management.ManagementFactory;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Executor for executing {@link EnvironmentCommand}s.
  */
 @Component
-public class EnvironmentCommandExecutor { //implements CommandExecutor {
+public class EnvironmentCommandExecutor implements CommandExecutor {
 
+    @Override
+    public boolean canExecute(Command command) {
+        return command.hasEnvironment();
+    }
 
+    @Override
+    public CommandResponse execute(Command command) {
+        if (!canExecute(command)) {
+            String exceptionMessage = "Invalid command type. Executor does not support commands of type " + command.getClass();
+            throw new IllegalArgumentException(exceptionMessage);
+        }
+        return CommandResponse
+                .newBuilder()
+                .setCommandId(command.getCommandId())
+                .setEnvironment(EnvironmentCommandResponse
+                        .newBuilder()
+                        .putAllEnvironmentVariables(System.getenv())
+                        .putAllSystemProperties(propertiesAsMap())
+                        .addAllJvmArguments(ManagementFactory.getRuntimeMXBean().getInputArguments())
+                )
+                .build();
+    }
 
+    private Map<String, String> propertiesAsMap() {
+        Map<String, String> propertiesAsMap = new HashMap<>();
+        for (String propertyName : System.getProperties().stringPropertyNames()) {
+            propertiesAsMap.put(propertyName, System.getProperty(propertyName));
+        }
+        return propertiesAsMap;
+    }
 }
--- inspectit-ocelot-core/src/test/java/rocks/inspectit/ocelot/core/command/handler/impl/EnvironmentCommandExecutorTest.java
+++ inspectit-ocelot-core/src/test/java/rocks/inspectit/ocelot/core/command/handler/impl/EnvironmentCommandExecutorTest.java
@@ -1,118 +1,100 @@
 package rocks.inspectit.ocelot.core.command.handler.impl;
 
-import org.apache.commons.io.FileUtils;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.TestPropertySource;
-import rocks.inspectit.ocelot.commons.models.command.impl.EnvironmentCommand;
-import rocks.inspectit.ocelot.commons.models.command.impl.LogsCommand;
-import rocks.inspectit.ocelot.config.model.command.AgentCommandSettings;
-import rocks.inspectit.ocelot.core.SpringTestBase;
-import rocks.inspectit.ocelot.core.config.InspectitEnvironment;
+import rocks.inspectit.ocelot.grpc.*;
 
-import java.io.File;
-import java.io.IOException;
-import java.net.URL;
-import java.nio.charset.Charset;
 import java.util.UUID;
-import java.util.concurrent.TimeUnit;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.awaitility.Awaitility.await;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @ExtendWith(MockitoExtension.class)
 public class EnvironmentCommandExecutorTest {
 
-    static final String tmpDir = "tmpconf_int_test";
-
-    static final String configurationA = "inspectit:\n  agent-commands:\n    enabled: true\n    url: http://localhost:8090/api/v1/agent/command";
-
-    @BeforeAll
-    static void createConfigDir() {
-        new File(tmpDir).mkdir();
-    }
-
-    @BeforeEach
-    void cleanConfigDir() throws Exception {
-        FileUtils.cleanDirectory(new File(tmpDir));
-    }
-
-    @AfterAll
-    static void deleteConfigDir() throws Exception {
-        FileUtils.deleteDirectory(new File(tmpDir));
-    }
+    @InjectMocks
+    private EnvironmentCommandExecutor executor;
 
     @Nested
-    @DirtiesContext
-    @TestPropertySource(properties = {"inspectit.config.file-based.path=" + tmpDir, "inspectit.config.file-based.frequency=50ms"})
-    public class Configuration extends SpringTestBase {
-
-        @Autowired
-        InspectitEnvironment env;
+    public class CanExecute {
 
         @Test
-        public void test() throws IOException {
-            testConfiguration(configurationA, true, new URL("http://localhost:8090/api/v1/agent/command"));
+        public void nonEnvironmentCommand() {
+            boolean result = executor.canExecute(Command.newBuilder()
+                    .setListClasses(ListClassesCommand.newBuilder())
+                    .build());
+
+            assertThat(result).isFalse();
         }
 
-        private void testConfiguration(String configuration, boolean enabled, URL commandUrl) throws IOException {
-            FileUtils.write(new File(tmpDir + "/env-command.yml"), configuration, Charset.defaultCharset());
+        @Test
+        public void environmentCommand() {
+            boolean result = executor.canExecute(Command.newBuilder()
+                    .setEnvironment(EnvironmentCommand.newBuilder())
+                    .build());
 
-            await().atMost(15, TimeUnit.SECONDS).untilAsserted(() -> {
-                AgentCommandSettings settings = env.getCurrentConfig().getAgentCommands();
-                assertThat(settings).isNotNull();
-                assertThat(settings.isEnabled()).isEqualTo(enabled);
-                assertThat(settings.getUrl()).isEqualTo(commandUrl);
-            });
+            assertThat(result).isTrue();
         }
-
     }
 
     @Nested
-    @DirtiesContext
     public class Execute {
 
-        @InjectMocks
-        EnvironmentCommandExecutor executor;
+        private Command environmentCommand;
 
-        @Test
-        public void testEnvironmentDetailsBeingBuiltCorrectly() {
-
-            EnvironmentCommand command = new EnvironmentCommand();
-            command.setCommandId(UUID.randomUUID());
-            EnvironmentCommand.Response response = (EnvironmentCommand.Response) executor.execute(command);
-
-            // available environment variables should be returned
-            assertThat(response.getEnvironment().getEnvironmentVariables().size()).isGreaterThan(0);
-            assertThat(response.getEnvironment().getEnvironmentVariables()).containsKey("PATH");
-
-            // system properties should be returned
-            assertThat(response.getEnvironment().getSystemProperties().size()).isGreaterThan(0);
-            assertThat(response.getEnvironment().getSystemProperties()).containsKey("os.name");
-            assertThat(response.getEnvironment().getSystemProperties()).containsKey("java.version");
-
-            // any jvm arguments passed in the starting command should be returned
-            assertThat(response.getEnvironment().getJvmArguments().size()).isGreaterThan(0);
+        @BeforeEach
+        public void createEnvironmentCommand() {
+            environmentCommand = Command.newBuilder()
+                    .setEnvironment(EnvironmentCommand.newBuilder())
+                    .setCommandId(UUID.randomUUID().toString())
+                    .build();
         }
 
         @Test
-        public void testInvalidCommand() {
-            LogsCommand command = new LogsCommand();
-            command.setCommandId(UUID.randomUUID());
+        public void failsOnNonEnvironmentCommand() {
+            Command nonEnvironmentCommand = Command.newBuilder()
+                    .setPing(PingCommand.newBuilder())
+                    .setCommandId(UUID.randomUUID().toString())
+                    .build();
 
-            IllegalArgumentException exception = Assertions.assertThrows(IllegalArgumentException.class, () -> executor.execute(command));
-            assertThat(exception.getMessage()).isEqualTo("Invalid command type. Executor does not support commands of type " + command.getClass());
+            assertThatThrownBy(() -> {
+                executor.execute(nonEnvironmentCommand);
+            }).isInstanceOf(IllegalArgumentException.class);
         }
 
+        @Test
+        public void returnsCommandIdFromCommand() {
+            CommandResponse response = executor.execute(environmentCommand);
+
+            assertThat(response.getCommandId()).isEqualTo(environmentCommand.getCommandId());
+        }
+
+        @Test
+        public void returnsEnvironmentVariables() {
+            EnvironmentCommandResponse environmentCommandResponse = executor.execute(environmentCommand)
+                    .getEnvironment();
+
+            assertThat(environmentCommandResponse.getEnvironmentVariablesMap()).containsKey("PATH");
+        }
+
+        @Test
+        public void returnsSystemProperties() {
+            EnvironmentCommandResponse environmentCommandResponse = executor.execute(environmentCommand)
+                    .getEnvironment();
+
+            assertThat(environmentCommandResponse.getSystemPropertiesMap()).containsKey("os.name");
+        }
+
+        @Test
+        public void returnsJvmArguments() {
+            EnvironmentCommandResponse environmentCommandResponse = executor.execute(environmentCommand)
+                    .getEnvironment();
+
+            assertThat(environmentCommandResponse.getJvmArgumentsList()).isNotEmpty();
+        }
     }
-
 }
