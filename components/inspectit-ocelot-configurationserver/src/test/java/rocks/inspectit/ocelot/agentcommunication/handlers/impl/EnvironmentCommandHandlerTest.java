af7fc6753ab6ae1e9e0e0098df5262ffc867236a
Dimi-Ma <dimi.m@gmx.de>
2022-11-09 15:49:57 +0100

    created Tests for EnvironmentCommandHandler

--- components/inspectit-ocelot-configurationserver/src/test/java/rocks/inspectit/ocelot/agentcommunication/handlers/impl/EnvironmentCommandHandlerTest.java
+++ components/inspectit-ocelot-configurationserver/src/test/java/rocks/inspectit/ocelot/agentcommunication/handlers/impl/EnvironmentCommandHandlerTest.java
@@ -1,23 +1,19 @@
 package rocks.inspectit.ocelot.agentcommunication.handlers.impl;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Answers;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.context.request.async.DeferredResult;
-import rocks.inspectit.ocelot.config.model.InspectitServerSettings;
-import rocks.inspectit.ocelot.grpc.Command;
-import rocks.inspectit.ocelot.grpc.PingCommand;
-
-import java.time.Duration;
+import rocks.inspectit.ocelot.grpc.CommandResponse;
+import rocks.inspectit.ocelot.grpc.EnvironmentCommandResponse;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class EnvironmentCommandHandlerTest {
@@ -25,51 +21,43 @@ public class EnvironmentCommandHandlerTest {
     @InjectMocks
     EnvironmentCommandHandler environmentCommandHandler;
 
-    @Mock(answer = Answers.RETURNS_DEEP_STUBS)
-    private InspectitServerSettings configuration;
-// TODO
-//    @Nested
-//    class PrepareResponse {
-//
-//        @Test
-//        public void cantHandleCommand() {
-//            Command command = null;
-//            String agentId = "test-id";
-//
-//            try {
-//                environmentCommandHandler.canHandle(PingCommand.newBuilder().build());
-//            } catch (IllegalArgumentException e) {
-//                assertThat(e.getMessage()).isEqualTo("EnvironmentCommandHandler can only handle commands of type EnvironmentCommand.");
-//            }
-//
-//        }
-//
-//        @Test
-//        public void preparesResponse() {
-//            when(configuration.getAgentCommand().getResponseTimeout()).thenReturn(Duration.ofMinutes(1));
-//            EnvironmentCommand command = new EnvironmentCommand();
-//            String agentId = "test-id";
-//
-//            DeferredResult<ResponseEntity<?>> result = environmentCommandHandler.prepareResponse(agentId, command);
-//
-//            assertThat(result).isNotNull();
-//
-//        }
-//
-//    }
-//
-//    @Nested
-//    class HandleResponse {
-//
-//        @Test
-//        public void handlesResponse() {
-//            EnvironmentCommand.Response mockResponse = mock(EnvironmentCommand.Response.class);
-//            DeferredResult<ResponseEntity<?>> result = new DeferredResult<>();
-//
-//            environmentCommandHandler.handleResponse(mockResponse, result);
-//
-//            assertThat(result.getResult()).isEqualTo(ResponseEntity.ok().build());
-//        }
-//    }
+    @Nested
+    class HandleResponse {
 
+        @Test
+        public void handlesResponse() {
+            EnvironmentCommandResponse response = EnvironmentCommandResponse
+                    .newBuilder()
+                    .putSystemProperties("key", "value")
+                    .build();
+
+            DeferredResult<ResponseEntity<?>> deferredResult = new DeferredResult<>();
+
+            environmentCommandHandler.handleResponse(CommandResponse.newBuilder()
+                    .setEnvironment(response)
+                    .build(), deferredResult);
+
+            ResponseEntity<String> result = (ResponseEntity<String>) deferredResult.getResult();
+            JsonObject obj = new JsonParser().parse(result.getBody()).getAsJsonObject();
+
+            assertThat(obj.get("systemProperties").getAsJsonObject().get("key").getAsString()).isEqualTo("value");
+        }
+
+        @Test
+        public void handlesResponseReturnsOK() {
+            EnvironmentCommandResponse response = EnvironmentCommandResponse
+                    .newBuilder()
+                    .putSystemProperties("key", "value")
+                    .build();
+
+            DeferredResult<ResponseEntity<?>> deferredResult = new DeferredResult<>();
+
+            environmentCommandHandler.handleResponse(CommandResponse.newBuilder()
+                    .setEnvironment(response)
+                    .build(), deferredResult);
+
+            ResponseEntity<String> result = (ResponseEntity<String>) deferredResult.getResult();
+            assertThat(result.getStatusCode()).isEqualTo(HttpStatus.OK);
+        }
+    }
 }
