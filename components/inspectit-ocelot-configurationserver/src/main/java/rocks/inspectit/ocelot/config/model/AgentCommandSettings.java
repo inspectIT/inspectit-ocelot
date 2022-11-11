9814baddb7789c1c520e875f6a2344c58e68ba77
Dimi-Ma <dimi.m@gmx.de>
2022-11-11 14:50:58 +0100

    increased grpc max inbound message size

--- components/inspectit-ocelot-configurationserver/src/main/java/rocks/inspectit/ocelot/config/model/AgentCommandSettings.java
+++ components/inspectit-ocelot-configurationserver/src/main/java/rocks/inspectit/ocelot/config/model/AgentCommandSettings.java
@@ -24,5 +24,5 @@ public class AgentCommandSettings {
      * Default is 4MiB which is also grpc's default.
      */
     @Builder.Default
-    private int maxInboundMessageSize = 4;
+    private int maxInboundMessageSize = 5000;
 }
