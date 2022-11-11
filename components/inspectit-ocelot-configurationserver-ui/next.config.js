2022d31ba9941db2aab222a3138bed3894a0d602
Priebe Daniel <danielpp1450@gmail.com>
2022-11-11 12:07:43 +0100

    Removed basePath, since it looks like the error, just occurs if the .yml file is empty

--- components/inspectit-ocelot-configurationserver-ui/next.config.js
+++ components/inspectit-ocelot-configurationserver-ui/next.config.js
@@ -3,7 +3,6 @@ const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = withCSS({
   distDir: '../.next',
-  basePath: '',
 
   // Each page will be exported as a directory
   trailingSlash: true,
