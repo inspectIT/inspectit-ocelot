65d249e5f199d8c968708a9b22bac47a9154e125
Jochen Just <jochen.just@novatec-gmbh.de>
2022-12-21 09:23:40 +0100

    Removes TODO after discussing

--- components/inspectit-ocelot-configdocsgenerator/src/test/java/inspectit/ocelot/configdocsgenerator/parsing/ConfigParserTest.java
+++ components/inspectit-ocelot-configdocsgenerator/src/test/java/inspectit/ocelot/configdocsgenerator/parsing/ConfigParserTest.java
@@ -64,10 +64,6 @@ class ConfigParserTest {
           // Build the expected result by hand
             Map<String, Boolean> enabledMap = new HashMap<>();
             enabledMap.put("free", true);
-          // TODO: Why was a mock used here? An upgrade to a newer Mockito version caused
-          // pollingSettingsMock.getFrequency to return a java.time.Duration of 0s instead of null.
-//            StandardPollingMetricsRecorderSettings pollingSettingsMock = Mockito.mock(StandardPollingMetricsRecorderSettings.class);
-//            when(pollingSettingsMock.getEnabled()).thenReturn(enabledMap);
             StandardPollingMetricsRecorderSettings pollingSettingsMock = new StandardPollingMetricsRecorderSettings();
             pollingSettingsMock.setEnabled(enabledMap);
             assertThat(result.getMetrics().getDisk()).usingRecursiveComparison().isEqualTo(pollingSettingsMock);
