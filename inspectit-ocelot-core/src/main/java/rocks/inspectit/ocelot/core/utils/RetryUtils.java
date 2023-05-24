112c7616a43e3f77ab975ea3833966feeda5e560
Jochen Just <jochen.just@novatec-gmbh.de>
2023-05-24 13:16:42 +0200

    Emits an info if a retry was necessary

--- inspectit-ocelot-core/src/main/java/rocks/inspectit/ocelot/core/utils/RetryUtils.java
+++ inspectit-ocelot-core/src/main/java/rocks/inspectit/ocelot/core/utils/RetryUtils.java
@@ -3,9 +3,15 @@ package rocks.inspectit.ocelot.core.utils;
 import io.github.resilience4j.core.IntervalFunction;
 import io.github.resilience4j.retry.Retry;
 import io.github.resilience4j.retry.RetryConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import rocks.inspectit.ocelot.config.model.config.RetrySettings;
 
+import java.time.temporal.ChronoUnit;
+
 public final class RetryUtils {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(RetryUtils.class);
 
     private RetryUtils() {
         // prevents initialization
@@ -21,7 +27,9 @@ public final class RetryUtils {
                                     retrySettings.getMultiplier().doubleValue(),
                                     retrySettings.getRandomizationFactor().doubleValue()))
                     .build();
-            return Retry.of(retryName, retryConfig);
+            Retry retry = Retry.of(retryName, retryConfig);
+            retry.getEventPublisher().onRetry( event -> LOGGER.info("Retrying for {} in {} ms.", retryName, event.getWaitInterval().get(ChronoUnit.MILLIS)));
+            return retry;
         }
         return null;
     }
