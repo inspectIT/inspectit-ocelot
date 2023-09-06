dad2a2c2615b9460c12b1047f89ce8ba4b544e92
EddeCCC <eduard.schander@gmx.de>
2023-09-06 08:24:22 +0200

    little refactor

--- inspectit-ocelot-core/src/main/java/rocks/inspectit/ocelot/core/instrumentation/context/wrapper/ScopeWrapper.java
+++ inspectit-ocelot-core/src/main/java/rocks/inspectit/ocelot/core/instrumentation/context/wrapper/ScopeWrapper.java
@@ -1,4 +1,4 @@
-package rocks.inspectit.ocelot.core.instrumentation.hook.wrapper;
+package rocks.inspectit.ocelot.core.instrumentation.context.wrapper;
 
 import io.opentelemetry.context.Scope;
 import lombok.AllArgsConstructor;
